b9bc77577227ab5c654005e0fa1d61afbb8dd6b3
davidevnes <davidantunes422@gmail.com>
2020-09-24 22:38:25 +0400

    Initiate React environment

---
+++ source/App/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import { browser, Tabs } from "webextension-polyfill-ts";
+
+import "./styles.scss";
+
+// function openWebPage(url: string): Promise<Tabs.Tab> {
+//   return browser.tabs.create({ url });
+// }
+
+const App: React.FC = () => {
+  return <section id="app">Stardust Collective</section>;
+};
+
+export default App;
