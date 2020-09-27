e54188e7827ffb3705d250a6621a9b58459c1c6b
davidevnes <davidantunes422@gmail.com>
2020-09-27 23:40:12 +0400

    refactor: update App

---
+++ source/containers/common/Container/Container.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import styles from './Container.scss';
+
+const Container = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default Container;
