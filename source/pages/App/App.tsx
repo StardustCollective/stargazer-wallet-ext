5a21d93b661464f3ee7e537f2ae715bbba1f60f9
devneser <zh_alex@yahoo.com>
2020-09-27 23:40:12 +0400

    refactor: update App

--- source/pages/App/App.tsx
+++ source/pages/App/App.tsx
@@ -1,14 +1,18 @@
 import React, { FC } from 'react';
 // import { browser, Tabs } from "webextension-polyfill-ts";
 
-import styles from './App.scss';
+import Container from 'containers/common/Container';
 
 // function openWebPage(url: string): Promise<Tabs.Tab> {
 //   return browser.tabs.create({ url });
 // }
 
 const App: FC = () => {
-  return <section id="app" className={styles.app}></section>;
+  return (
+    <section id="app">
+      <Container></Container>
+    </section>
+  );
 };
 
 export default App;
