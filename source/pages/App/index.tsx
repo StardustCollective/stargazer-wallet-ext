d47cfc60565e9e7fb64b4c9925a668316f2ee3e9
devneser <zh_alex@yahoo.com>
2020-09-29 15:42:49 +0400

    refactor: update App/index.tsx

--- source/pages/App/index.tsx
+++ source/pages/App/index.tsx
@@ -1,6 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from '../../redux/store';
+import App from './App';
 
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("popup-root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('popup-root')
+);
