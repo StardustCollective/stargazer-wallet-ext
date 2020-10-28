a67b4cb705991ea35fe3bc2b1d82e462b57abd91
devneser <zh_alex@yahoo.com>
2020-10-28 20:28:45 +0400

    feat: add fade transition

--- source/routers/UnAuth.tsx
+++ source/routers/UnAuth.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 import Start from 'containers/unauth/Start';
 import Remind from 'containers/unauth/Remind';
 import CreatePass from 'containers/unauth/CreatePass';
+import { useTransition, animated } from 'react-spring';
 import {
   CreatePhrase,
   ConfirmPhrase,
@@ -10,19 +11,54 @@ import {
 } from 'containers/unauth/Phrase';
 
 const UnAuth = () => {
+  const location = useLocation();
+  const transitions = useTransition(location, (locat) => locat.pathname, {
+    initial: { opacity: 1 },
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: { duration: 500 },
+  });
+
   /**
    * --- Create Account Flow ---
    * Start => Remind => CreatePass => RemindPhrase => CreatePhrase => ConfirmPhrase
    */
   return (
-    <Switch>
-      <Route path="/app.html" component={Start} exact />
-      <Route path="/remind" component={Remind} exact />
-      <Route path="/create/pass" component={CreatePass} exact />
-      <Route path="/create/phrase/remind" component={RemindPhrase} exact />
-      <Route path="/create/phrase/generated" component={CreatePhrase} exact />
-      <Route path="/create/phrase/check" component={ConfirmPhrase} exact />
-    </Switch>
+    <>
+      {transitions.map(({ item, props, key }) => (
+        <animated.div
+          style={{
+            ...props,
+            position: 'absolute',
+            height: '100%',
+            width: '100%',
+          }}
+          key={key}
+        >
+          <Switch location={item}>
+            <Route path="/app.html" component={Start} exact />
+            <Route path="/remind" component={Remind} exact />
+            <Route path="/create/pass" component={CreatePass} exact />
+            <Route
+              path="/create/phrase/remind"
+              component={RemindPhrase}
+              exact
+            />
+            <Route
+              path="/create/phrase/generated"
+              component={CreatePhrase}
+              exact
+            />
+            <Route
+              path="/create/phrase/check"
+              component={ConfirmPhrase}
+              exact
+            />
+          </Switch>
+        </animated.div>
+      ))}
+    </>
   );
 };
 
