43b537c8996ff8479ad254a26445ac893f933fb8
toparc077 <andyhoogup@gmail.com>
2021-01-04 21:20:25 +0100

    UI: PhraseView

--- source/containers/auth/Settings/views/GeneralView/index.tsx
+++ source/containers/auth/Settings/views/GeneralView/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import ListIcon from 'assets/images/svg/list.svg';
 import InfoIcon from 'assets/images/svg/info.svg';
 import DeleteIcon from 'assets/images/svg/trash.svg';
+import { useSettingsView } from 'hooks/index';
+
 import styles from './index.scss';
+import { PHRASE_VIEW } from '../routes';
 
 const GeneralView = () => {
+  const showView = useSettingsView();
+
   return (
     <div className={styles.general}>
       <ul>
-        <li>
+        <li onClick={() => showView(PHRASE_VIEW)}>
           <img src={ListIcon} alt="user" />
           Wallet seed phrase
         </li>
--- source/containers/auth/Settings/views/PhraseView/index.tsx
+++ source/containers/auth/Settings/views/PhraseView/index.tsx
@@ -1,15 +1,37 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
 import TextInput from 'components/TextInput';
-import React from 'react';
+import { useCopyClipboard } from 'hooks/index';
 
 import styles from './index.scss';
 
 const PhraseView = () => {
+  const [checked, setChecked] = useState(true);
+  const [isCopied, copyText] = useCopyClipboard();
+  const seedClass = clsx(styles.seed, {
+    [styles.copied]: isCopied,
+    [styles.notAllowed]: !checked,
+  });
+
+  const handleCopySeed = () => {
+    if (!checked) return;
+    copyText('Seed phrase');
+  };
+
   return (
     <div className={styles.phrase}>
       <span>Please enter your wallet password:</span>
-      <TextInput type="password" visiblePassword fullWidth />
+      <TextInput
+        type="password"
+        visiblePassword
+        fullWidth
+        variant={styles.input}
+      />
       <span>Click to copy your seed phrase:</span>
-      <div className={styles.seed}>Seed phrase</div>
+      <div className={seedClass} onClick={handleCopySeed}>
+        **** ******* ****** ****** ****** ******** *** ***** ****** ***** *****
+        ****** ******* ****** *****
+      </div>
       <span>
         Warning: Keep your seed phrase secret! Anyone with your seed phrase can
         access any account connected to this wallet and steal your assets.
