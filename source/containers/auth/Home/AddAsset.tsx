033dda2e6aa92995ed94ba52f90f38e6fb6f1127
assasinOOZ <assasinooz77@gmail.com>
2021-03-01 06:42:44 +0800

    feat: complete search asset page with mock data(search is also simulated)

--- source/containers/auth/Home/AddAsset.tsx
+++ source/containers/auth/Home/AddAsset.tsx
@@ -1,22 +1,67 @@
-import React, { useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 import TextInput from 'components/TextInput';
 import styles from './AddAsset.scss';
+import SearchIcon from 'assets/images/svg/search.svg';
+
+import mockAssets from './mockData';
+import { Asset } from './types';
 
 const AddAsset = () => {
-  const [tokenName, setTokenName] = useState('');
+  const [filteredAssets, setFilteredAssets] = useState<Array<Asset>>();
+  const [keyword, setKeyword] = useState('');
 
+  useEffect(() => {
+    setFilteredAssets(
+      mockAssets.filter((asset: Asset) => asset.name.includes(keyword))
+    );
+  }, [keyword]);
   return (
     <section className={styles.addAsset}>
-      <TextInput
-        placeholder="Search by name ticker or contract"
-        fullWidth
-        value={tokenName}
-        name="address"
-        // inputRef={register}
-        onChange={(e) => setTokenName(e.target.value)}
-        // variant={addressInputClass}
-      />
+      <div className={styles.searchInput}>
+        <img src={SearchIcon} />
+        <TextInput
+          placeholder="Search by name ticker or contract"
+          fullWidth
+          value={keyword}
+          name="address"
+          // inputRef={register}
+          onChange={(e) => setKeyword(e.target.value)}
+          // variant={addressInputClass}
+        />
+      </div>
+      <div className={styles.assets}>
+        <ul>
+          {filteredAssets &&
+            filteredAssets.map((asset: Asset) => {
+              return (
+                <Fragment key={uuid()}>
+                  <li
+                    onClick={() => {
+                      // TODO
+                    }}
+                  >
+                    <div>
+                      <div className={styles.iconWrapper}>
+                        <img src={asset.logo}></img>
+                      </div>
+                      <span>{asset.name}</span>
+                    </div>
+                    <div>
+                      <span>
+                        <span>
+                          {asset.balance}
+                          <b>{asset.shortName}</b>
+                        </span>
+                      </span>
+                    </div>
+                  </li>
+                </Fragment>
+              );
+            })}
+        </ul>
+      </div>
     </section>
   );
 };
