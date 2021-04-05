c99bc49c1939f7aee44305221a48f498a4714e7c
devneser <zh_alex@yahoo.com>
2021-04-05 22:26:54 +0400

    refactor: resolve json parse issue

--- source/containers/auth/Settings/views/ImportAccountView/index.tsx
+++ source/containers/auth/Settings/views/ImportAccountView/index.tsx
@@ -58,26 +58,32 @@ const ImportAccountView = () => {
       fileReader.readAsText(jsonFile, 'UTF-8');
       fileReader.onload = (ev: ProgressEvent<FileReader>) => {
         if (ev.target) {
-          const json = JSON.parse(ev.target.result as string);
-          if (!dag4.keyStore.isValidJsonPrivateKey(json)) {
+          try {
+            const json = JSON.parse(ev.target.result as string);
+            if (!dag4.keyStore.isValidJsonPrivateKey(json)) {
+              alert.removeAll();
+              alert.error('Error: Invalid private key json file');
+              return;
+            }
+
+            setLoading(true);
+            dag4.keyStore
+              .decryptPrivateKey(json, data.password)
+              .then((privKey: string) => {
+                handleImportPrivKey(privKey, data.label);
+              })
+              .catch(() => {
+                alert.removeAll();
+                alert.error(
+                  'Error: Invalid password to decrypt private key json file'
+                );
+                setLoading(false);
+              });
+          } catch (error) {
             alert.removeAll();
             alert.error('Error: Invalid private key json file');
-            return;
+            setLoading(false);
           }
-
-          setLoading(true);
-          dag4.keyStore
-            .decryptPrivateKey(json, data.password)
-            .then((privKey: string) => {
-              handleImportPrivKey(privKey, data.label);
-            })
-            .catch(() => {
-              alert.removeAll();
-              alert.error(
-                'Error: Invalid password to decrypt private key json file'
-              );
-              setLoading(false);
-            });
         }
       };
     } else {
