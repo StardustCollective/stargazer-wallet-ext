7c430f5160f362fee4ccc5577bec66cbba2b6a5d
devneser <zh_alex@yahoo.com>
2021-05-14 14:54:30 +0400

    feat: add confirm page

--- source/containers/confirm/WalletConnect/index.tsx
+++ source/containers/confirm/WalletConnect/index.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import Button from 'components/Button';
-import React from 'react';
+import React, { useState } from 'react';
 
 import IconButton from '@material-ui/core/IconButton';
 import LinkIcon from '@material-ui/icons/CallMade';
@@ -8,7 +8,14 @@ import Checkbox from '@material-ui/core/Checkbox';
 
 import styles from './index.module.scss';
 
+enum ConnectWallet {
+  Choose,
+  Confirm,
+}
+
 const WalletConnect = () => {
+  const [step, setStep] = useState(ConnectWallet.Confirm);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.frame}>
@@ -16,25 +23,40 @@ const WalletConnect = () => {
           <img src={styles.logo} />
           <span>https://dag.chad</span>
         </section>
-        <div className={styles.title}>Connect With Stargazer Wallet</div>
-        <label>Please select an amount:</label>
-        <section className={styles.accounts}>
-          <table>
-            <tr>
-              <td>
-                <Checkbox color="primary" size="small" />
-              </td>
-              <td>1</td>
-              <td>0xb2...a49D</td>
-              <td>0.003020 ETH</td>
-              <td>
-                <IconButton size="small">
-                  <LinkIcon fontSize="small" />
-                </IconButton>
-              </td>
-            </tr>
-          </table>
-        </section>
+        <div className={styles.title}>
+          {step === ConnectWallet.Choose
+            ? `Connect With Stargazer Wallet`
+            : `Connect to Main Wallet (DAG...1v04)`}
+        </div>
+        <label>
+          {step === ConnectWallet.Choose
+            ? `Please select an amount:`
+            : `Allows this site to:`}
+        </label>
+        {step === ConnectWallet.Confirm ? (
+          <section className={styles.confirm}>
+            <Checkbox color="primary" size="small" checked />
+            View the addresses of your permitted accounts (required)
+          </section>
+        ) : (
+          <section className={styles.accounts}>
+            <table>
+              <tr>
+                <td>
+                  <Checkbox color="primary" size="small" />
+                </td>
+                <td>1</td>
+                <td>0xb2...a49D</td>
+                <td>0.003020 ETH</td>
+                <td>
+                  <IconButton size="small">
+                    <LinkIcon fontSize="small" />
+                  </IconButton>
+                </td>
+              </tr>
+            </table>
+          </section>
+        )}
         <label>Only connect to sites you trust</label>
         <section className={styles.actions}>
           <Button
