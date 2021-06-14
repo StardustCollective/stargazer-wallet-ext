abaf8c7fc615fb6bc9b8fbdfd32e4aa132041a49
devneser <zh_alex@yahoo.com>
2021-06-14 12:10:01 +0400

    feat: update send screen

---
+++ source/components/Slider/Slider.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import MUISlider, { SliderProps } from '@material-ui/core/Slider';
+
+import styles from './Slider.scss';
+
+interface ISlider extends Partial<SliderProps> {
+  variant?: string;
+}
+
+const Slider: FC<ISlider> = ({ variant, ...otherProps }) => {
+  return (
+    <MUISlider
+      className={variant}
+      classes={{ thumb: styles.thumb }}
+      {...otherProps}
+    />
+  );
+};
+
+export default Slider;
