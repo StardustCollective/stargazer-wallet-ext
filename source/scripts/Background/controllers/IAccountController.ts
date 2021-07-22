f2fdae5b57af2da46d0137f1be7ee05c7431c8c0
Frank Fox <ffox77@gmail.com>
2021-07-21 23:15:44 -0700

    refactor for eth tx history

--- source/scripts/Background/controllers/IAccountController.ts
+++ source/scripts/Background/controllers/IAccountController.ts
@@ -13,7 +13,7 @@ export interface IAccountController {
   // unsubscribeAccount: (index: number, pwd: string) => boolean;
   // addNewAccount: (label: string) => Promise<string | null>;
   updateTxs: (limit?: number, searchAfter?: string) => Promise<void>;
-  getFullETHTxs: () => any[];
+  getFullETHTxs: () => ITransactionInfo[];
   updateWalletLabel: (walletId: string, label: string) => void;
   updateAccountActiveAsset: (asset: IAssetState) => void;
   addNewToken: (address: string) => Promise<void>;
