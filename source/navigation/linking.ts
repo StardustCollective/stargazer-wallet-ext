ef5c76bd433d2958b45a93ce2328537bca41d4e8
Cesar Miranda <cmirandac665@gmail.com>
2021-08-03 10:45:19 -0700

    Refactored: Routes to seperate file

--- source/navigation/linking.ts
+++ source/navigation/linking.ts
@@ -1,33 +1,16 @@
 ///////////////////////////
-// Screens
+// Routes
 ///////////////////////////
-
-import screens from './screens';
+ 
+import routes from './routes';
 
 ///////////////////////////
 // Linking Configs
 ///////////////////////////
 
 const config = {
-    screens: {
-      //Common
-      [screens.common.import]: '/import',
-      // UnAuth Stack
-      [screens.unAuthorized.home]: 'app.html',
-      [screens.unAuthorized.remind]: '/remind',
-      [screens.unAuthorized.createPass]: '/create/pass',
-      [screens.unAuthorized.createPhraseGenerated]: '/create/phrase/generated',
-      [screens.unAuthorized.createPhraseRemind]: '/create/phrase/remind',
-      [screens.unAuthorized.createPhraseCheck]: '/create/phrase/check',
-      // Auth Stack
-      [screens.authorized.home]: '/home',
-      [screens.authorized.addAsset]: '/asset/add',
-      [screens.authorized.asset]: '/asset',
-      [screens.authorized.sendConfirm]: '/send/confirm',
-      [screens.authorized.send]: '/send/:address?',
-      [screens.authorized.gasSettings]: '/gas-settings',
-    },
-  };
+    screens: routes
+};
   
 const linking = {
     prefixes: ['https://', 'http://'],
