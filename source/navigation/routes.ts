e96c6a1d61061097a03ed987816ba7fb292c70df
Cesar Miranda <cmirandac665@gmail.com>
2021-08-03 10:44:36 -0700

    Added: react-navigation routes

---
+++ source/navigation/routes.ts
@@ -0,0 +1,29 @@
+
+///////////////////////////
+// Screens
+///////////////////////////
+
+import screens from './screens';
+
+///////////////////////////
+// Routes
+///////////////////////////
+
+export default {
+  //Common
+  [screens.common.import]: '/import',
+  // UnAuth Stack
+  [screens.unAuthorized.home]: '/homeUnauth',
+  [screens.unAuthorized.remind]: '/remind',
+  [screens.unAuthorized.createPass]: '/create/pass',
+  [screens.unAuthorized.createPhraseGenerated]: '/create/phrase/generated',
+  [screens.unAuthorized.createPhraseRemind]: '/create/phrase/remind',
+  [screens.unAuthorized.createPhraseCheck]: '/create/phrase/check',
+  // Auth Stack
+  [screens.authorized.home]: '/home',
+  [screens.authorized.addAsset]: '/asset/add',
+  [screens.authorized.asset]: '/asset',
+  [screens.authorized.sendConfirm]: '/send/confirm',
+  [screens.authorized.send]: '/send/:address?',
+  [screens.authorized.gasSettings]: '/gas-settings',
+};
