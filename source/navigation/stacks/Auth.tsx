9bfdc64c3249f6ec080d40f790946ac75ef7c8c4
Cesar Miranda <cmirandac665@gmail.com>
2021-08-04 17:18:52 -0700

    Added: Default titles and refactored routes

--- source/navigation/stacks/Auth.tsx
+++ source/navigation/stacks/Auth.tsx
@@ -35,20 +35,29 @@ import { createStackNavigator } from '@react-navigation/stack';
 ///////////////////////////
 
 const Stack = createStackNavigator();
+const SCREEN_DEFAULT_TITLE_STRINGS = {
+  wallet: 'Wallet',
+  import: 'Import',
+  addAsset: 'Add Asset',
+  asset: 'Asset',
+  confirm: 'Confirm',
+  send: 'Send',
+  gasSettings: 'Gas Settings'
+}
 
 const Auth = () => {
 
   const controller = useController();
   const isUnlocked = controller.wallet.isUnlocked();
   const redirectRoute = controller.appRoute();
-  const lockedRoute =  screens.authorized.start;
+  const lockedRoute = screens.authorized.start;
 
   let unlockedRoute = screens.authorized.home;
 
   // If there is a pending confirmation for a send
   // the user will be returned to the confirm screen
   // if they exit and return to the app.
-   if (
+  if (
     redirectRoute === screens.authorized.sendConfirm &&
     controller.wallet.account.getTempTx()
   ) {
@@ -61,14 +70,18 @@ const Auth = () => {
     <Stack.Navigator
       screenOptions={{ animationEnabled: true }}
       initialRouteName={initialRoute}>
-      {!isUnlocked && <Stack.Screen options={{headerShown: false}} name={screens.authorized.start} component={Start} />}
-      {isUnlocked && <Stack.Screen name={screens.authorized.home} component={Home} />}
-      {isUnlocked && <Stack.Screen name={screens.common.import} component={Import} />}
-      {isUnlocked && <Stack.Screen name={screens.authorized.addAsset} component={AddAsset} />}
-      {isUnlocked && <Stack.Screen name={screens.authorized.asset} component={Asset} />}
-      {isUnlocked && <Stack.Screen name={screens.authorized.sendConfirm} component={SendConfirm} />}
-      {isUnlocked && <Stack.Screen name={screens.authorized.send} component={Send} />}
-      {isUnlocked && <Stack.Screen name={screens.authorized.gasSettings} component={GasSettings} />}
+      {!isUnlocked && <Stack.Screen options={{ headerShown: false }} name={screens.authorized.start} component={Start} />}
+      { isUnlocked && (
+        <>
+          <Stack.Screen options={{ title: SCREEN_DEFAULT_TITLE_STRINGS.wallet }} name={screens.authorized.home} component={Home} />
+          <Stack.Screen options={{ title: SCREEN_DEFAULT_TITLE_STRINGS.import }} name={screens.common.import} component={Import} />
+          <Stack.Screen options={{ title: SCREEN_DEFAULT_TITLE_STRINGS.addAsset }} name={screens.authorized.addAsset} component={AddAsset} />
+          <Stack.Screen options={{ title: SCREEN_DEFAULT_TITLE_STRINGS.asset }} name={screens.authorized.asset} component={Asset} />
+          <Stack.Screen options={{ title: SCREEN_DEFAULT_TITLE_STRINGS.confirm }} name={screens.authorized.sendConfirm} component={SendConfirm} />
+          <Stack.Screen options={{ title: SCREEN_DEFAULT_TITLE_STRINGS.send }} name={screens.authorized.send} component={Send} />
+          <Stack.Screen options={{ title: SCREEN_DEFAULT_TITLE_STRINGS.gasSettings }} name={screens.authorized.gasSettings} component={GasSettings} />
+        </>)
+      }
     </Stack.Navigator>
   );
 };
