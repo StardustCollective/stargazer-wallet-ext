c5d43c385d35aabab41dc834582320db9d5d2d1e
Cesar Miranda <cmirandac665@gmail.com>
2021-08-11 10:24:38 -0700

    Fixed merge conflicts

--- source/pages/App/App.tsx
+++ source/pages/App/App.tsx
@@ -1,26 +1,39 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from 'navigation/stacks/Auth';
 import UnAuthStack from 'navigation/stacks/UnAuth';
 import { RootState } from 'state/store';
 import IVaultState from 'state/vault/types';
+import linking from 'navigation/linking';
+import { useController } from 'hooks/index';
 
 import 'assets/styles/global.scss';
 
 const App: FC = () => {
 
+  const controller = useController();
+
   const { wallets, hasEncryptedVault, migrateWallet }: IVaultState = useSelector(
     (state: RootState) => state.vault
   );
 
+  const onNavigationStateChange = (state: any) => {
+    const routes = state.routes;
+    const currentRoute = routes[routes.length - 1];
+    const currentRouteName = currentRoute.name;
+    // Store the route name, it will be used at start up to
+    // preserve the screen state.
+    controller.appRoute(currentRouteName);
+  }
+
   return (
-    <NavigationContainer>
-      {migrateWallet || (wallets && Object.values(wallets).length > 0) || hasEncryptedVault ? (
-        <AuthStack />
-      ) : (
-        <UnAuthStack />
-      )}
+    <NavigationContainer linking={linking} onStateChange={onNavigationStateChange}>
+          {migrateWallet || (wallets && Object.values(wallets).length > 0) || hasEncryptedVault ? (
+            <AuthStack />
+          ) : (
+            <UnAuthStack />
+          )}
     </NavigationContainer>
   );
 };
