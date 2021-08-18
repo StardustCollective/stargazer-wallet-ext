a8f2f00e4091c766982c91ef181d513878312264
Cesar Miranda <cmirandac665@gmail.com>
2021-08-17 19:22:09 -0700

    Added: React-native mobiel project to source

---
+++ source/native/ios/Pods/Target Support Files/Pods-stargazerWalletMobile-stargazerWalletMobileTests/Pods-stargazerWalletMobile-stargazerWalletMobileTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_stargazerWalletMobile_stargazerWalletMobileTests : NSObject
+@end
+@implementation PodsDummy_Pods_stargazerWalletMobile_stargazerWalletMobileTests
+@end
