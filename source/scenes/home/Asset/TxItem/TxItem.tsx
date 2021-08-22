f44661b2ddeac897668f656092cb97de7d591d2c
Cesar Miranda <cmirandac665@gmail.com>
2021-08-22 13:28:21 -0700

    Refactored: Moved the transaction item rendering to its own component

---
+++ source/scenes/home/Asset/TxItem/TxItem.tsx
@@ -0,0 +1,144 @@
+///////////////////////
+// Modules
+///////////////////////
+
+import React, { FC } from 'react';
+import { formatDistanceDate } from '../../helpers';
+
+///////////////////////
+// Components
+///////////////////////
+
+import Spinner from '@material-ui/core/CircularProgress';
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
+import LinkIcon from '@material-ui/icons/OpenInNewOutlined';
+
+///////////////////////
+// Styles
+///////////////////////
+
+import styles from './TxItem.scss'
+
+///////////////////////
+// Types
+///////////////////////
+
+import { Transaction } from 'state/vault/types';
+
+///////////////////////
+// Interfaces
+///////////////////////
+
+interface ITxItem {
+  key: number;
+  tx: Transaction;
+  isSelf: boolean;
+  isReceived: boolean;
+  isETH: boolean;
+  isETHPending: boolean;
+  showGroupBar: boolean;
+  txTypeLabel: string;
+  currencySymbol: string;
+  amount: string;
+  fiatAmount: string;
+  onItemClick?: (hash: string) => void;
+}
+
+///////////////////////
+// Component
+///////////////////////
+
+const TxItem: FC<ITxItem> = ({
+  key,
+  tx,
+  isETH,
+  isSelf,
+  isReceived,
+  isETHPending,
+  showGroupBar,
+  txTypeLabel,
+  currencySymbol,
+  amount,
+  fiatAmount,
+  onItemClick,
+}) => {
+
+  const handleOpenExplorer = () => {
+
+    if(onItemClick) {
+      onItemClick(isETHPending ? tx.txHash : tx.hash);
+    }
+  
+  }
+
+  const RenderIcon: FC = () => {
+    if (!isETH) {
+      return (
+        <>
+          {tx.checkpointBlock ? (
+            isReceived ? (
+              <DoubleArrowIcon className={styles.recvIcon} />
+            ) : (
+              <DoubleArrowIcon />
+            )
+          ) : (
+            <Spinner size={16} className={styles.spinner} />
+          )}
+        </>
+      );
+    }
+    return (
+      <>
+        {!tx.assetId ? (
+          isReceived ? (
+            <DoubleArrowIcon className={styles.recvIcon} />
+          ) : (
+            <DoubleArrowIcon />
+          )
+        ) : (
+          <Spinner size={16} className={styles.spinner} />
+        )}
+      </>
+    );
+  };
+  
+
+  return (
+    <div key={key} className={styles.content}>
+      {showGroupBar && (
+        <div className={styles.groupBar}>
+          <span>{formatDistanceDate(tx.timestamp)}</span>
+        </div>
+      )}
+      <div className={styles.txItem} onClick={handleOpenExplorer}>
+        <div className={styles.txIcon}>
+          <RenderIcon />
+        </div>
+        <div className={styles.txInfo}>
+          <div>
+            <span>
+              {isSelf ? 'Self' : (isReceived ? 'Received' : 'Sent')}
+            </span>
+          </div>
+          <div>
+            <small>
+              {txTypeLabel}
+            </small>
+          </div>
+        </div>
+        <div className={styles.txAmount}>
+          <span>{amount}<b>{currencySymbol}</b></span>
+          <small>{fiatAmount}</small>
+        </div>
+        <div className={styles.txExplorerIcon}>
+          <div className={styles.circle}>
+            <LinkIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+}
+
+export default TxItem;
--- source/scenes/home/Asset/TxsPanel.tsx
+++ source/scenes/home/Asset/TxsPanel.tsx
@@ -1,21 +1,17 @@
-import React, { FC, Fragment, useCallback, useState } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import clsx from 'clsx';
-import { v4 as uuid } from 'uuid';
 import { useFiat } from 'hooks/usePrice';
 import { useSelector } from 'react-redux';
-import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
 import GoTopIcon from '@material-ui/icons/VerticalAlignTop';
-import LinkIcon from '@material-ui/icons/OpenInNewOutlined';
 import IconButton from '@material-ui/core/IconButton';
-import Spinner from '@material-ui/core/CircularProgress';
 
 import { useController } from 'hooks/index';
-import { formatDistanceDate } from '../helpers';
 import StargazerIcon from 'assets/images/svg/stargazer.svg';
 import { DAG_EXPLORER_SEARCH, ETH_NETWORK } from 'constants/index';
 import { RootState } from 'state/store';
 import IVaultState, { AssetType, Transaction } from 'state/vault/types';
 import IAssetListState from 'state/assets/types';
+import TxItem from './TxItem';
 
 import styles from './Asset.scss';
 import { KeyringNetwork } from '@stardust-collective/dag4-keyring';
@@ -76,42 +72,12 @@ const TxsPanel: FC<ITxsPanel> = ({ address, transactions }) => {
       isETH ? `${ethUrl}tx/${tx}` : `${DAG_EXPLORER_SEARCH}${tx}`,
       '_blank'
     );
+    
   };
 
   const handleGoTop = () => {
     scrollArea!.scrollTo({ top: 0, behavior: 'smooth' });
     setShowed(false);
-  };
-
-  const renderIcon = (isReceived: boolean, tx: any) => {
-    if (!isETH) {
-      return (
-        <>
-          {tx.checkpointBlock ? (
-            isReceived ? (
-              <DoubleArrowIcon className={styles.recvIcon} />
-            ) : (
-              <DoubleArrowIcon />
-            )
-          ) : (
-            <Spinner size={16} className={styles.spinner} />
-          )}
-        </>
-      );
-    }
-    return (
-      <>
-        {!tx.assetId ? (
-          isReceived ? (
-            <DoubleArrowIcon className={styles.recvIcon} />
-          ) : (
-            <DoubleArrowIcon />
-          )
-        ) : (
-          <Spinner size={16} className={styles.spinner} />
-        )}
-      </>
-    );
   };
 
   return (
@@ -128,106 +94,69 @@ const TxsPanel: FC<ITxsPanel> = ({ address, transactions }) => {
         )}
       </div>
       {transactions.length ? (
-        <>
-          <ul>
-            {transactions.map((tx: Transaction, idx: number) => {
-              const isETHPending = isETH && tx.assetId === activeAsset.id;
-              const isReceived =
-                (!isETH && tx.receiver === address) ||
-                (isETH && !tx.assetId && tx.to && tx.to[0].to.toLowerCase() === address.toLowerCase()) ||
-                (isETHPending && tx.toAddress.toLowerCase() === address.toLowerCase());
-              const isSent =
-                (!isETH && tx.sender === address) ||
-                (isETH && !tx.assetId && tx.from && tx.from[0].from.toLowerCase() === address.toLowerCase()) ||
-                (isETHPending && tx.fromAddress.toLowerCase() === address.toLowerCase());
-              const isSelf = isSent && isReceived;
+        <div>
+          {transactions.map((tx: Transaction, idx: number) => {
+            const isETHPending = isETH && tx.assetId === activeAsset.id;
+            const isReceived =
+              (!isETH && tx.receiver === address) ||
+              (isETH && !tx.assetId && tx.to && tx.to[0].to.toLowerCase() === address.toLowerCase()) ||
+              (isETHPending && tx.toAddress.toLowerCase() === address.toLowerCase());
+            const isSent =
+              (!isETH && tx.sender === address) ||
+              (isETH && !tx.assetId && tx.from && tx.from[0].from.toLowerCase() === address.toLowerCase()) ||
+              (isETHPending && tx.fromAddress.toLowerCase() === address.toLowerCase());
+            const isSelf = isSent && isReceived;
+            const txTypeLabel = isReceived ? `From: ${isETHPending ? tx.fromAddress
+              : isETH
+                ? tx.from && tx.from[0].from
+                : tx.sender
+              }`
+              : `To: ${isETHPending
+                ? tx.toAddress
+                : isETH
+                  ? tx.to && tx.to[0].to
+                  : tx.receiver
+              }`
+            const amount = isETH ? Number(isETHPending ? tx.amount : tx.balance).toFixed(4)
+              : (tx.amount / 1e8).toFixed(4)
+            const fiatAmount = isETH ? getFiatAmount(Number(isETHPending ? tx.amount : tx.balance), 2)
+              : getFiatAmount(tx.amount / 1e8, 8)
 
-              return (
-                <Fragment key={uuid()}>
-                  {isShowedGroupBar(tx, idx) && (
-                    <li className={styles.groupbar}>
-                      {formatDistanceDate(tx.timestamp)}
-                    </li>
-                  )}
-                  <li
-                    onClick={() =>
-                      handleOpenExplorer(isETHPending ? tx.txHash : tx.hash)
-                    }
-                  >
-                    <div>
-                      <div className={styles.iconWrapper}>
-                        {renderIcon(isReceived && !isSelf, tx)}
-                      </div>
-                      <span>
-                        {isSelf ? 'Self' : (isReceived ? 'Received' : 'Sent')}
-                        {!isSelf && (
-                          <small>
-                            {isReceived
-                              ? `From: ${
-                                isETHPending
-                                  ? tx.fromAddress
-                                  : isETH
-                                    ? tx.from && tx.from[0].from
-                                    : tx.sender
-                              }`
-                              : `To: ${
-                                isETHPending
-                                  ? tx.toAddress
-                                  : isETH
-                                    ? tx.to && tx.to[0].to
-                                    : tx.receiver
-                              }`}
-                          </small>
-                        )}
-                      </span>
-                    </div>
-                    <div>
-                      <span>
-                        <span>
-                          {isETH
-                            ? Number(
-                                isETHPending ? tx.amount : tx.balance
-                              ).toFixed(4)
-                            : (tx.amount / 1e8).toFixed(4)}{' '}
-                          <b>{assets[activeAsset.id].symbol}</b>
-                        </span>
-                        <small>
-                          {isETH
-                            ? getFiatAmount(
-                                Number(isETHPending ? tx.amount : tx.balance),
-                                2
-                              )
-                            : getFiatAmount(tx.amount / 1e8, 8)}
-                        </small>
-                      </span>
-                      <div className={styles.linkIcon}>
-                        <LinkIcon />
-                      </div>
-                    </div>
-                  </li>
-                </Fragment>
-              );
-            })}
-          </ul>
-        </>
-      ) : (
-        <>
-          <span className={styles.noTxComment}>
-            {`You have no transaction history, send or receive $${
-              assets[activeAsset.id].symbol
-            } to register
+            return (
+                <TxItem
+                  tx={tx}
+                  isETH={isETH}
+                  isSelf={isSelf}
+                  isReceived={isReceived}
+                  isETHPending={isETHPending}
+                  showGroupBar={isShowedGroupBar(tx, idx)}
+                  txTypeLabel={txTypeLabel}
+                  currencySymbol={assets[activeAsset.id].symbol}
+                  amount={amount}
+                  fiatAmount={fiatAmount}
+                  onItemClick={handleOpenExplorer}
+                />
+            );
+          })}
+        </div>
+  ) : (
+    <>
+      <span className={styles.noTxComment}>
+        {`You have no transaction history, send or receive $${assets[activeAsset.id].symbol
+          } to register
             your first transaction.`}
-          </span>
-          <img
-            src={'/'+StargazerIcon}
-            className={styles.stargazer}
-            alt="stargazer"
-            height="167"
-            width="auto"
-          />
-        </>
-      )}
-    </section>
+      </span>
+      <img
+        src={'/' + StargazerIcon}
+        className={styles.stargazer}
+        alt="stargazer"
+        height="167"
+        width="auto"
+      />
+    </>
+  )
+}
+    </section >
   );
 };
 
