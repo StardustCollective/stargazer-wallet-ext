f31f55793193d8473fc53e6952738d7b91e1d6a4
Cesar Miranda <cmirandac665@gmail.com>
2021-08-27 12:31:27 -0700

    Added: New Card Component

---
+++ source/components/Card/Card.tsx
@@ -0,0 +1,35 @@
+///////////////////////
+// Modules
+///////////////////////
+
+import React from 'react';
+
+///////////////////////
+// Styles
+///////////////////////
+
+import styles from './Card.scss';
+
+///////////////////////
+// Interface
+///////////////////////
+
+interface ICardProps {
+  children: React.ReactNode;
+}
+
+///////////////////////
+// Component
+///////////////////////
+
+const Card = ({ children }: ICardProps) => {
+
+    return (
+      <div className={styles.card}>
+        {children}
+      </div>
+    );
+
+}
+
+export default Card;
