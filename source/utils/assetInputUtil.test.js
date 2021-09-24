2948179ee2983036f91b3301d5d11da003a88db3
Cesar Miranda <cmirandac665@gmail.com>
2021-09-24 00:25:27 -0700

    Added: assetInputUtil and dummy test

---
+++ source/utils/assetInputUtil.test.js
@@ -0,0 +1,7 @@
+
+
+describe("Decimals", () => {
+    test('Dummy Test', () => {
+        expect(1).toBe(1);
+    })
+})
---
+++ source/utils/assetInputUtil.ts
@@ -0,0 +1,24 @@
+
+export const getDecimalPlaces = (valString: string): Number => {
+  return valString.includes('.') ? valString.split('.')[1].length : 0;
+};
+
+export const inputValToString = (targetVal: string): string => {
+  const floatVal = parseFloat(targetVal);
+
+  return isNaN(floatVal) ? '0' : targetVal;
+};
+
+export const getChangeAmount = (
+  targetVal: string,
+  maxAmount: number,
+  assetDecimals: number
+): string | undefined => {
+  const valString = inputValToString(targetVal);
+  const decimalPlaces = getDecimalPlaces(valString);
+
+  if (parseFloat(valString) > maxAmount || decimalPlaces > assetDecimals)
+    return;
+
+  return valString;
+};
