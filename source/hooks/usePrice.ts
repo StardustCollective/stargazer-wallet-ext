83b5be444f98d791eb42f30fba60084d2306db1a
Cesar Miranda <cmirandac665@gmail.com>
2021-10-11 16:03:23 -0700

    Fixed: Issue where missing asset would cause a crash

--- source/hooks/usePrice.ts
+++ source/hooks/usePrice.ts
@@ -21,7 +21,7 @@ export function useFiat(currencyName = true, asset?: IAssetInfoState ) {
   );
 
   return (amount: number, fraction = 4, basePriceId?: string) => {
-    const priceId = basePriceId ||  asset ? asset.priceId : assets[activeAsset.id].priceId
+    const priceId = basePriceId ||  !!asset ? asset?.priceId : assets[activeAsset.id].priceId
     const value =
       amount * (priceId ? fiat[basePriceId || priceId]?.price || 0 : 0);
     return `${currencyName ? currency.symbol : ''}${
