47b3d4d7f799d37922bd2730deb3493b51eb79a0
Trini Le <trinile@gmail.com>
2021-11-10 21:11:07 -0800

    return user rejected error if result is false

--- source/scripts/ContentScript/inject/stargazerProvider.txt.ts
+++ source/scripts/ContentScript/inject/stargazerProvider.txt.ts
@@ -9,19 +9,19 @@ const REQUEST_MAP = {
     blockNumber: SUPPORTED_WALLET_METHODS.getBlockNumber,
     estimateGas: SUPPORTED_WALLET_METHODS.estimateGas,
     sendTransaction: SUPPORTED_WALLET_METHODS.sendTransaction,
-    signMessage: SUPPORTED_WALLET_METHODS.signMessage,
+    signMessage: SUPPORTED_WALLET_METHODS.signMessage
   },
   DAG: {
     chainId: SUPPORTED_WALLET_METHODS.getChainId,
     accounts: SUPPORTED_WALLET_METHODS.getAccounts,
-    chainId: SUPPORTED_WALLET_METHODS.sendTransaction,
-    getBalance: SUPPORTED_WALLET_METHODS.getBalance,
+    sendTransaction: SUPPORTED_WALLET_METHODS.sendTransaction,
+    getBalance: SUPPORTED_WALLET_METHODS.getBalance
   },
   isConnected: SUPPORTED_WALLET_METHODS.isConnected,
   getNetwork: SUPPORTED_WALLET_METHODS.getNetwork,
   getAddress: SUPPORTED_WALLET_METHODS.getAddress,
-  getBalance: SUPPORTED_WALLET_METHODS.getBalance,
-}
+  getBalance: SUPPORTED_WALLET_METHODS.getBalance
+};
 
 const SUPPORTED_CHAINS = {
   constellation: {
@@ -45,7 +45,7 @@ const ERRORS = {
     err.code = 4200;
     return err;
   }
-}
+};
 
 async function handleRequest(chain, req) {
   const asset = SUPPORTED_CHAINS[chain].asset;
@@ -55,7 +55,7 @@ async function handleRequest(chain, req) {
   let [prefix, method] = req.method.split('_');
 
   if (prefix && prefix.toLowerCase() !== SUPPORTED_CHAINS[chain].prefix) {
-    throw ERRORS.INVALID_METHOD()
+    throw ERRORS.INVALID_METHOD();
   }
 
   if (!method) {
@@ -69,7 +69,7 @@ async function handleRequest(chain, req) {
   if (method === 'requestAccounts') {
     const { result, data } = await window.providerManager.enable();
 
-    if (!result) throw ERRORS.USER_REJECTED()
+    if (!result) throw ERRORS.USER_REJECTED();
 
     return data.accounts;
   }
@@ -79,22 +79,27 @@ async function handleRequest(chain, req) {
     REQUEST_MAP.hasOwnProperty(prefix) &&
     REQUEST_MAP[prefix].hasOwnProperty(method)
   ) {
-    return provider.getMethod(REQUEST_MAP[prefix][method])(...req.params);
+    const response = await provider.getMethod(REQUEST_MAP[prefix][method])(
+      ...req.params
+    );
+
+    if (!response.result) throw ERRORS.USER_REJECTED();
+    return response;
   }
 
   throw ERRORS.INVALID_METHOD();
 }
 
-const provider = (chain) => {
+const provider = chain => {
   chain = chain.toLowerCase();
-  
+
   if (!Object.keys(SUPPORTED_CHAINS).includes(chain)) {
     console.error('Unsupported chain: ' + chain);
   }
 
   return {
-    request: async (req) => {
-      const params = req.params || []
+    request: async req => {
+      const params = req.params || [];
       return await handleRequest(chain, {
         method: req.method,
         params
@@ -103,70 +108,74 @@ const provider = (chain) => {
     on: (method, callback) => {
       let origin = window.location.hostname;
       if (window.location.port) {
-        origin += ":" + window.location.port;
+        origin += ':' + window.location.port;
       }
-  
-      const id = chain + "." + origin + "." + method;
-  
+
+      const id = chain + '.' + origin + '.' + method;
+
       window.stargazer._listeners[id] = ({ detail }) => {
         if (detail) {
           callback(JSON.parse(detail));
         }
       };
-  
-      window.addEventListener(
-        id,
-        window.stargazer._listeners[id],
-        { passive: true }
-      );
-  
+
+      window.addEventListener(id, window.stargazer._listeners[id], {
+        passive: true
+      });
+
       // Register the origin of the listening site.
-      window.postMessage({ id, type: 'STARGAZER_EVENT_REG', data: { method, origin, chain } }, '*');
+      window.postMessage(
+        { id, type: 'STARGAZER_EVENT_REG', data: { method, origin, chain } },
+        '*'
+      );
     },
-    removeListener: (method) => {
+    removeListener: method => {
       let origin = window.location.hostname;
       if (window.location.port) {
-        origin += ":" + window.location.port;
+        origin += ':' + window.location.port;
       }
-  
-      const id = chain + "." + origin + "." + method;
-  
+
+      const id = chain + '.' + origin + '.' + method;
+
       if (window.stargazer._listeners[id]) {
         window.removeEventListener(id, window.stargazer._listeners[id]);
-  
+
         delete window.stargazer._listeners[id];
       }
-  
-      window.postMessage({ id, type: 'STARGAZER_EVENT_DEREG', data: { method, origin, chain } }, '*');
+
+      window.postMessage(
+        { id, type: 'STARGAZER_EVENT_DEREG', data: { method, origin, chain } },
+        '*'
+      );
     }
-  }
-}
+  };
+};
 
 const allPrivider = provider('ethereum');
 
 window.stargazer = {
   evtRegMap: {},
   version: 1,
-  getProvider: (chain) => provider(chain),
+  getProvider: chain => provider(chain),
   isConnected: async () => {
     const provider = window.providerManager.getProviderFor('main');
-    return provider.getMethod('wallet.isConnected')()
+    return provider.getMethod('wallet.isConnected')();
   },
   enable: async () => {
-    const { result, data } = await window.providerManager.enable()
+    const { result, data } = await window.providerManager.enable();
 
-    if (!result) throw ERRORS.USER_REJECTED()
+    if (!result) throw ERRORS.USER_REJECTED();
 
     return data.accounts;
   },
-  request: async (req) => {
+  request: async req => {
     return allPrivider.request(req);
   },
   on: (method, callback) => {
     return allPrivider.on(method, callback);
   },
-  removeListener: (method) => {
+  removeListener: method => {
     return allPrivider.removeListener(method);
   },
   _listeners: {}
-}
+};
