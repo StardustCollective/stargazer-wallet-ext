009900fd7f327aa51bccca8ebdbf8a282d5c2c54
Cesar Miranda <cmirandac665@gmail.com>
2021-11-11 16:34:32 -0800

    Removed: global.process is now stubbed via process/browser.js module

--- source/native/shim.ts
+++ source/native/shim.ts
@@ -1,9 +1,2 @@
-// global.process: Not the best solution, but good for now to make React Native happy.
-global.process = {
-  version : '',
-  env: {
-    NODE_ENV : '',
-  }
-}
-
+global.process = require('process/browser.js');
 global.localStorage = require('@react-native-async-storage/async-storage').default;
