6805c1eba0377595922a4f72dc6d2e609fb7d64e
Cesar Miranda <cmirandac665@gmail.com>
2021-11-15 15:49:18 -0800

    Added: Place holder for unauth start screen

---
+++ source/scenes/unauth/Start/Start.native.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView } from 'react-native';
+import { View, Text } from 'react-native';
+
+const Start = ({ navigation }) => {
+  return (
+    <SafeAreaView>
+      <Text>What is going on?</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Start;
