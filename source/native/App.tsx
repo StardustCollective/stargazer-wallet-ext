dbed63b8b76fb8a5b7d8b74ce34b37f59a0d74cd
Cesar Miranda <cmirandac665@gmail.com>
2021-11-17 21:08:11 -0800

    Added: SafeAreaProvider, NativeBaseProvider

--- source/native/App.tsx
+++ source/native/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NativeBaseProvider } from 'native-base';
 import RootStack from 'navigation/stacks/Root';
 import linking from 'navigation/linking';
 import Store from 'state/store';
@@ -7,11 +9,15 @@ import { Provider } from 'react-redux';
 
 const App = () => {
   return (
-    <Provider store={Store}>
-      <NavigationContainer linking={linking}>
-        <RootStack />
-      </NavigationContainer>
-    </Provider>
+    <SafeAreaProvider>
+      <NativeBaseProvider>
+        <Provider store={Store}>
+          <NavigationContainer linking={linking}>
+            <RootStack />
+          </NavigationContainer>
+        </Provider>
+      </NativeBaseProvider>
+    </SafeAreaProvider >
   );
 };
 
