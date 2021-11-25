e814016b5af2c07e7965571ee10fa5673c775851
Cesar Miranda <cmirandac665@gmail.com>
2021-11-24 17:08:15 -0800

    Migrated: Link component

---
+++ source/components/Link/Link.native.tsx
@@ -0,0 +1,45 @@
+import React, { ReactNode, FC } from 'react';
+import { 
+  StyleSheet,
+  Text,
+  TouchableOpacity
+} from 'react-native';
+
+
+import styles from './styles';
+
+interface ILink {
+  id?: string;
+  color?: 'primary' | 'secondary' | 'monotoneOne';
+  title: string,
+  onPress?: () => void;
+  extraStyles?: string;
+}
+
+const Link: FC<ILink> = ({
+  id,
+  color = 'primary',
+  onPress,
+  extraStyles,
+  title
+}) => {
+
+  const flatLinkStyles = StyleSheet.flatten([
+    styles.link,
+    styles[color],
+  ])
+
+  const composedLinkStyles = StyleSheet.compose(flatLinkStyles, extraStyles)
+
+  return (
+    <>
+      <TouchableOpacity testID={id} onPress={onPress}>
+        <Text style={composedLinkStyles}>
+          {title}
+        </Text>
+      </TouchableOpacity>
+    </>
+  );
+};
+
+export default Link;
