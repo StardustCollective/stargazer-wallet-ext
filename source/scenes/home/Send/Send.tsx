e3c4b198132e68e3a8a6ca18f4616ba4a4c296b8
Trini <trinile@users.noreply.github.com>
2021-12-17 07:22:31 -0800

    Development/trini/sgw 46/transactions rejected handling (#163)
    * Add cancel events to send and approveSpend scenes
    
    * return user rejected error if result is false
    
    * update message handler requests to pass in ev.detail.result in event callback
    
    * add RPC error
    
    * throw error if result is false
    
    * update StargazerProvider to throw object with code and message

--- source/scenes/home/Send/Send.tsx
+++ source/scenes/home/Send/Send.tsx
@@ -14,6 +14,7 @@ import { useSelector } from 'react-redux';
 import { useAlert } from 'react-alert';
 import Slider from '@material-ui/core/Slider';
 import queryString from 'query-string';
+import { browser } from 'webextension-polyfill-ts';
 import { useHistory } from "react-router-dom";
 import useGasEstimate from 'hooks/useGasEstimate';
 
@@ -38,7 +39,6 @@ import IAssetListState from 'state/assets/types';
 import { BigNumber, ethers } from 'ethers';
 import { ITransactionInfo } from '../../../scripts/types';
 import { getChangeAmount } from 'utils/sendUtil';
-
 import sendHeader from 'navigation/headers/send';
 
 interface IWalletSend {
@@ -215,8 +215,13 @@ const WalletSend: FC<IWalletSend> = ({ initAddress = '', navigation }) => {
     }
   };
 
-  const handleClose = () => {
-    if (isExternalRequest) {
+  const handleClose = async() => {
+    if(isExternalRequest){
+      const cancelEvent = new CustomEvent('transactionSent', {detail: {windowId, approved: false, result: false }});
+      const background = await browser.runtime.getBackgroundPage();
+
+      background.dispatchEvent(cancelEvent);
+
       window.close();
     } else {
       linkTo('/asset');
@@ -255,8 +260,8 @@ const WalletSend: FC<IWalletSend> = ({ initAddress = '', navigation }) => {
         !fee ||
         !address
       );
-    } 
-    
+    }
+
     return (
       !isValidAddress ||
       !amount ||
