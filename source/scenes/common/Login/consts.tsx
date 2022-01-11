1c7174fa2f39c6ed81a0e02bfa2e8967b3a3f959
Chin Okoronkwo <chinwendu.okoronkwo@gmail.com>
2022-01-11 12:21:53 -0500

    additon of login status comments

--- source/scenes/common/Login/consts.tsx
+++ source/scenes/common/Login/consts.tsx
@@ -1,5 +1,8 @@
 import * as Yup from 'yup';
 
+export const LOGIN_SUCCESS_COMMENT = `Login successful.`;
+export const LOGIN_FAILURE_COMMENT = `Error: Invalid password`;
+
 export const schema = Yup.object().shape({
   password: Yup.string().required('Error: Password is a required field.'),
   // .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/),
