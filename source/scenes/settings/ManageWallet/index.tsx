169637d06ac7241eace7d4612ea3515bcde22d2b
Cesar Miranda <cmirandac665@gmail.com>
2022-01-10 23:44:19 -0800

    Refactored: Recovery phrase and private key retrieval from ledger accounts in MnageWallet scene

--- source/scenes/settings/ManageWallet/index.tsx
+++ source/scenes/settings/ManageWallet/index.tsx
@@ -13,7 +13,7 @@ import TextInput from 'components/TextInput';
 import { useController } from 'hooks/index';
 
 import styles from './index.scss';;
-import { KeyringWalletState, KeyringWalletType} from '@stardust-collective/dag4-keyring';
+import { KeyringWalletState, KeyringWalletType } from '@stardust-collective/dag4-keyring';
 import { useSelector } from 'react-redux';
 import { useLinkTo } from '@react-navigation/native';
 import WalletSelectors from 'selectors/walletsSelectors';
@@ -22,12 +22,12 @@ interface IManageWalletView {
   navigation: any;
 }
 
-const ManageWallet: FC<IManageWalletView> = ({route, navigation}) => {
+const ManageWallet: FC<IManageWalletView> = ({ route, navigation }) => {
   const controller = useController();
   const linkTo = useLinkTo();
   const id = route.params.id;
   const { handleSubmit, register } = useForm();
-  const wallets: KeyringWalletState[]  = useSelector(WalletSelectors.selectAllWallets);
+  const wallets: KeyringWalletState[] = useSelector(WalletSelectors.selectAllWallets);
   const wallet = wallets.find(w => w.id === id);
 
   const onSubmit = (data: any) => {
@@ -51,41 +51,60 @@ const ManageWallet: FC<IManageWalletView> = ({route, navigation}) => {
     linkTo(`/settings/wallets/privateKey?id=${id}`)
   }
 
+  const RenderBackupSection = () => {
+
+    return (
+      <>
+        {wallet.type === KeyringWalletType.MultiChainWallet ?
+          (
+            <div className={styles.menu}
+              onClick={onShowRecoveryPhraseClicked}
+            >
+              <Icon Component={SeedIcon} />
+              <span>Show Recovery Phrase</span>
+              <ArrowIcon />
+            </div>
+          ) :
+          (
+            <div
+              className={styles.menu}
+              onClick={onShowPrivateKeyClicked}
+            >
+              <Icon Component={PrivKeyIcon} />
+              <span>Export private key</span>
+              <ArrowIcon />
+            </div>
+          )
+        }
+      </>
+    )
+  }
+
   return (
     <form className={styles.wrapper} onSubmit={handleSubmit(onSubmit)}>
-      <label>Name</label>
-      <TextInput
-        name="name"
-        visiblePassword
-        fullWidth
-        variant={styles.input}
-        defaultValue={wallet.label}
-        inputRef={register({ required: true })}
-      />
-      <label>Backup Options</label>
-      {wallet.type === KeyringWalletType.MultiChainWallet ? (
-        <section className={styles.menu} 
-        onClick={onShowRecoveryPhraseClicked}
-        >
-          <Icon Component={SeedIcon} />
-          <span>Show Recovery Phrase</span>
-          <ArrowIcon />
-        </section>
-      ) : (
-        <section
-          className={styles.menu}
-          onClick={onShowPrivateKeyClicked}
-        >
-          <Icon Component={PrivKeyIcon} />
-          <span>Export private key</span>
-          <ArrowIcon />
-        </section>
-      )}
-
-      <span>
-        If you lose access to this wallet, your funds will be lost, unless you
-        back up!
-      </span>
+      <section>
+        <label>Name</label>
+        <TextInput
+          name="name"
+          visiblePassword
+          fullWidth
+          variant={styles.input}
+          defaultValue={wallet.label}
+          inputRef={register({ required: true })}
+        />
+      </section>
+      {wallet.type !== KeyringWalletType.LedgerAccountWallet &&
+        (
+          <section>
+            <label>Backup Options</label>
+            <RenderBackupSection />
+            <div className={styles.backupWarningText}>
+              If you lose access to this wallet, your funds will be lost, unless you
+              back up!
+            </div>
+          </section>
+        )
+      }
       <section
         className={styles.menu}
         onClick={onDeleteWalletClicked}
@@ -96,18 +115,17 @@ const ManageWallet: FC<IManageWalletView> = ({route, navigation}) => {
       </section>
       <section className={styles.actions}>
         <div className={styles.buttons}>
-        <Button
-          type="button"
-          variant={clsx(styles.button, styles.cancel)}
-          onClick={onCancelClicked}
-        >
-          Cancel
-        </Button>
-        <Button type="submit" variant={clsx(styles.button, styles.save)}>
-          Save
-        </Button>
+          <Button
+            type="button"
+            variant={clsx(styles.button, styles.cancel)}
+            onClick={onCancelClicked}
+          >
+            Cancel
+          </Button>
+          <Button type="submit" variant={clsx(styles.button, styles.save)}>
+            Save
+          </Button>
         </div>
-
       </section>
     </form>
   );
