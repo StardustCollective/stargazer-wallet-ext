0228539ff83da97e7246392b685f1b15a10c0f7b
Cesar Miranda <cmirandac665@gmail.com>
2022-01-14 01:53:20 -0800

    Migrated: Layout component to react native

---
+++ source/scenes/common/Layout/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from 'assets/styles/_variables';
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+  heading: {
+    backgroundColor: COLORS.grey_light,
+    paddingVertical: 18,
+    paddingHorizontal: 24,
+  },
+
+});
+
+export default styles;
