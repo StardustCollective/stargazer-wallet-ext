8d607b3883b17114e9772d3c7f59fe8a4608dfb5
Cesar Miranda <cmirandac665@gmail.com>
2022-01-14 01:53:59 -0800

    Removed: Uneeded Import

--- source/scenes/unauth/Start/styles.ts
+++ source/scenes/unauth/Start/styles.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-import { COLORS, FONTS } from 'assets/styles/_variables';
+import { COLORS } from 'assets/styles/_variables';
 
 const styles = StyleSheet.create({
   layout: {
