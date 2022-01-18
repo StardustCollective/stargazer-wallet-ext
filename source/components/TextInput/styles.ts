12de24b42fd235f9607bf2e5b855e1ffb95514e7
Cesar Miranda <cmirandac665@gmail.com>
2022-01-18 13:06:10 -0800

    Changed: Renamed TextInput styles file

--- source/components/TextInput/styles.ts
+++ source/components/TextInput/styles.ts
@@ -2,12 +2,16 @@ import { StyleSheet } from 'react-native';
 import { FONTS, FONT_WEIGHTS, COLORS } from 'assets/styles/_variables';
 
 const styles = StyleSheet.create({
+  input: {
+    fontSize: 15,
+  },
   inputContainer: {
     backgroundColor: COLORS.white,
     borderWidth: 1,
     borderRadius: 6,
     paddingLeft: 10,
     borderColor: COLORS.grey_100,
+    marginBottom: -15
   },
   label: {
     paddingLeft: 14,
