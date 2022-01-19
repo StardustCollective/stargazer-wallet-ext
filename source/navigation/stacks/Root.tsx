b77113e9415101ee704b2bdc004365c8b79a9d26
Trini Le <trinile@gmail.com>
2022-01-19 12:09:55 -0800

    Revert "sandbox settings to revert"
    This reverts commit a2a059b7fee1f18194044119e75c651209205415.

--- source/navigation/stacks/Root.tsx
+++ source/navigation/stacks/Root.tsx
@@ -1,55 +1,52 @@
-/// ////////////////////////
+///////////////////////////
 // Modules
-/// ////////////////////////
+///////////////////////////
 
 import React from 'react';
 import { RootState } from 'state/store';
 import IVaultState from 'state/vault/types';
 
-/// ////////////////////////
+///////////////////////////
 // Hooks
-/// ////////////////////////
+///////////////////////////
 
 import { useSelector } from 'react-redux';
 
-/// ////////////////////////
+///////////////////////////
 // Stacks
-/// ////////////////////////
+///////////////////////////
 
-// import HomeStack from './Home';
-// import UnAuthStack from './UnAuth';
+import HomeStack from './Home';
+import UnAuthStack from './UnAuth';
 
-import Sandbox from 'scenes/sandbox';
-/// ////////////////////////
+///////////////////////////
 // Screens Names
-/// ////////////////////////
-
-/// ////////////////////////
-// Navigation
-/// ////////////////////////
-
-import { createStackNavigator } from '@react-navigation/stack';
-import defaultHeader from 'navigation/headers/default';
+///////////////////////////
 
 import screens from '../screens';
 
-/// ////////////////////////
+///////////////////////////
+// Navigation
+///////////////////////////
+
+import { createStackNavigator } from '@react-navigation/stack';
+import defaultHeader from 'navigation/headers/default'
+
+///////////////////////////
 // Constants
-/// ////////////////////////
+///////////////////////////
 
 const Stack = createStackNavigator();
 
 const Root = () => {
+
   let initialRoute = screens.unAuthorized.root;
 
-  const { wallets, hasEncryptedVault, migrateWallet }: IVaultState =
-    useSelector((state: RootState) => state.vault);
+  const { wallets, hasEncryptedVault, migrateWallet }: IVaultState = useSelector(
+    (state: RootState) => state.vault
+  );
 
-  if (
-    migrateWallet ||
-    (wallets && Object.values(wallets).length > 0) ||
-    hasEncryptedVault
-  ) {
+  if(migrateWallet || (wallets && Object.values(wallets).length > 0) || hasEncryptedVault ){
     initialRoute = screens.authorized.root;
   }
 
@@ -58,12 +55,9 @@ const Root = () => {
       screenOptions={(navigation) => ({
         ...defaultHeader(navigation),
       })}
-    >
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name={screens.unAuthorized.root}
-        component={Sandbox}
-      />
+      initialRouteName={initialRoute}>
+      <Stack.Screen options={{ headerShown: false }} name={screens.unAuthorized.root} component={UnAuthStack} />
+      <Stack.Screen options={{ headerShown: false }} name={screens.authorized.root} component={HomeStack} />
     </Stack.Navigator>
   );
 };
