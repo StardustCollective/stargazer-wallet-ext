4f08b8b7b11ed3e2fdd574941d4da2f16742c4c0
Trini Le <trinile@gmail.com>
2022-01-20 15:19:28 -0800

    update Card style to match existing margins

--- source/components/Card/styles.ts
+++ source/components/Card/styles.ts
@@ -6,13 +6,13 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     background: COLORS.white,
     boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.08)',
+    margin: 0,
     marginBottom: 10,
   },
   wrapperStyle: {
     flex: 1,
     flexDirection: 'row',
-    // height: 80,
-  }
+  },
 });
 
 export default styles;
