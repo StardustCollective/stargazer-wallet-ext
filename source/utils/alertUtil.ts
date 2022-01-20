c31781358fbfdd381fc86b8ca3ea8c75b74a3175
Trini Le <trinile@gmail.com>
2022-01-20 09:04:39 -0800

    fix alertUtil

--- source/utils/alertUtil.ts
+++ source/utils/alertUtil.ts
@@ -3,27 +3,25 @@ Wrapper to handle alerts between mobile and web
 */
 
 import { Platform } from 'react-native';
-import { showMessage } from 'react-native-flash-message';
+import { showMessage, MessageType } from 'react-native-flash-message';
 import { useAlert } from 'react-alert';
 
-exports.showAlert = ( message:string, type:string | undefined ) => {
+
+exports.showAlert = ( message:string, type:MessageType | undefined | null ) => {
     if( Platform.OS === 'web' ) {
         const alert = useAlert();
 
         alert.removeAll();
 
-        if( type === 'error' ) {
+        if( type === 'danger' ) {
             alert.error(message);
         } else {
             alert.show(message);
         }
     } else {
-        const options= {
+        showMessage({
             message,
             type
-        };
-
-        showMessage(options);
+        });
     }
 };
-
