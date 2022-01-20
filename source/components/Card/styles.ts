1a745485964b066abe870a109abdf3d0a432a41e
Trini Le <trinile@gmail.com>
2022-01-20 14:55:46 -0800

    update Card styles

--- source/components/Card/styles.ts
+++ source/components/Card/styles.ts
@@ -2,13 +2,16 @@ import { StyleSheet } from 'react-native';
 import { COLORS } from 'assets/styles/_variables';
 
 const styles = StyleSheet.create({
-  card: {
-    display: 'flex',
-    // height: 80,
+  containerStyle: {
     borderRadius: 8,
     background: COLORS.white,
     boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.08)',
     marginBottom: 10,
+  },
+  wrapperStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    // height: 80,
   }
 });
 
