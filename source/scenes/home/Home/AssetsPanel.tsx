3f0aa2a4efea0e6802b71a0b3254125122795f3d
Alex Brandes <alex@getdor.com>
2022-01-21 13:48:35 -0800

    Refactored active network assets code into a selector

--- source/scenes/home/Home/AssetsPanel.tsx
+++ source/scenes/home/Home/AssetsPanel.tsx
@@ -1,52 +1,48 @@
 import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
-import { v4 as uuid } from 'uuid';
+// import { v4 as uuid } from 'uuid';
 import { useLinkTo } from '@react-navigation/native';
+// import { KeyringNetwork } from '@stardust-collective/dag4-keyring';
 
 import AssetItem from 'components/AssetItem';
 import StargazerIcon from 'assets/images/svg/stargazer.svg';
 import { RootState } from 'state/store';
-import IVaultState, { AssetType, IAssetState } from 'state/vault/types';
+import IVaultState, { IAssetState } from 'state/vault/types';
 import { useController } from 'hooks/index';
+import walletSelectors from 'selectors/walletsSelectors';
 
 import styles from './Home.scss';
 import IAssetListState from '../../../state/assets/types';
-import { KeyringNetwork } from '@stardust-collective/dag4-keyring';
+
 
 const AssetsPanel: FC = () => {
   const controller = useController();
   const linkTo = useLinkTo();
-  const { activeWallet, activeNetwork }: IVaultState = useSelector(
+  const { activeWallet }: IVaultState = useSelector(
     (state: RootState) => state.vault
   );
   const assets: IAssetListState = useSelector(
     (state: RootState) => state.assets
   );
 
+  const activeNetworkAssets = useSelector(walletSelectors.selectActiveNetworkAssets);
+
   const handleSelectAsset = async (asset: IAssetState) => {
     await controller.wallet.account.updateAccountActiveAsset(asset);
     linkTo('/asset');
   };
 
+  console.log('activeNetworkAssets: ', activeNetworkAssets);
+
   const renderAssetList = () => {
     return (
       <>
-        {Object.values(activeWallet.assets)
-          .filter(
-            (asset) =>
-              assets[asset.id].network === 'both' ||
-              assets[asset.id].network ===
-              activeNetwork[
-              asset.type === AssetType.Constellation
-                ? KeyringNetwork.Constellation
-                : KeyringNetwork.Ethereum
-              ]
-          )
-          .map((asset) => {
+        {activeNetworkAssets
+          .map((asset: any) => {
             return (
               <AssetItem
                 id={asset.id}
-                key={uuid()}
+                key={asset.id}
                 asset={asset}
                 assetInfo={assets[asset.id]}
                 itemClicked={() => handleSelectAsset(asset)}
--- source/selectors/walletsSelectors.ts
+++ source/selectors/walletsSelectors.ts
@@ -2,85 +2,107 @@
  * Handles derived data for wallets state.
  */
 
-/////////////////////////
+/// //////////////////////
 // Modules
-/////////////////////////
+/// //////////////////////
 
 import { RootState } from 'state/store';
 import { createSelector } from 'reselect';
-import {
-  KeyringNetwork,
-  KeyringWalletState,
-} from '@stardust-collective/dag4-keyring';
+import { KeyringNetwork, KeyringWalletState, KeyringAssetInfo } from '@stardust-collective/dag4-keyring';
 
-/////////////////////////
+/// //////////////////////
 // Types
-/////////////////////////
+/// //////////////////////
 
-import { IAccountDerived } from 'state/vault/types';
+import { IAccountDerived, IWalletState, AssetType, IAssetState, ActiveNetwork } from 'state/vault/types';
 
-/////////////////////////
+/// //////////////////////
 // Selectors
-/////////////////////////
+/// //////////////////////
 
 /**
  * Returns root wallets state
  */
-const wallets = (state: RootState) => state.vault.wallets;
+const getWallets = (state: RootState) => state.vault.wallets;
+
+/**
+ * Returns activeWallet state
+ */
+const getActiveWallet = (state: RootState) => state.vault.activeWallet;
+
+/**
+ * Returns activeNetwork state
+ */
+const getActiveNetwork = (state: RootState) => state.vault.activeNetwork;
+
+/**
+ * Returns assets
+ */
+const getAssets = (state: RootState) => state.assets;
 
 /**
  * Returns all accounts from all wallets.
  */
 
-const selectAllAccounts = createSelector(
-  wallets, 
-  (wallets: KeyringWalletState[]) => {
-    let allAccounts = [];
-    for (let i = 0; i < wallets.length; i++) {
-      let accounts = wallets[i].accounts
-      for (let j = 0; j < wallets[i].accounts.length; j++) {
-        let account = accounts[j] as IAccountDerived;
-        account.label = wallets[i].label;
-        allAccounts.push(account);
-      }
+const selectAllAccounts = createSelector(getWallets, (wallets: KeyringWalletState[]) => {
+  const allAccounts = [];
+  for (let i = 0; i < wallets.length; i++) {
+    const { accounts } = wallets[i];
+    for (let j = 0; j < wallets[i].accounts.length; j++) {
+      const account = accounts[j] as IAccountDerived;
+      account.label = wallets[i].label;
+      allAccounts.push(account);
     }
-    return allAccounts;
-  });
+  }
+  return allAccounts;
+});
 
 /**
  * Returns all DAG accounts from all wallets.
  */
-const selectAllDagAccounts = createSelector(
-  selectAllAccounts,
-  (allAccounts: IAccountDerived[]) => {
-    return allAccounts.filter(
-      (account) => account.network === KeyringNetwork.Constellation
-    );
-  }
-);
+const selectAllDagAccounts = createSelector(selectAllAccounts, (allAccounts: IAccountDerived[]) => {
+  return allAccounts.filter((account) => account.network === KeyringNetwork.Constellation);
+});
 
 /**
  * Returns all ETH accounts from all wallets.
  */
-const selectAllEthAccounts = createSelector(
-  selectAllAccounts,
-  (allAccounts: IAccountDerived[]) => {
-    return allAccounts.filter(
-      (account) => account.network === KeyringNetwork.Ethereum
+const selectAllEthAccounts = createSelector(selectAllAccounts, (allAccounts: IAccountDerived[]) => {
+  return allAccounts.filter((account) => account.network === KeyringNetwork.Ethereum);
+});
+
+const selectAllWallets = createSelector(getWallets, (wallets: KeyringWalletState[]) => {
+  return [...wallets];
+});
+
+const selectActiveNetworkAssets = createSelector(
+  getActiveWallet,
+  getActiveNetwork,
+  getAssets,
+  (activeWallet: IWalletState, activeNetwork: ActiveNetwork, assets: KeyringAssetInfo[]): IAssetState[] => {
+    if (!activeWallet?.assets) {
+      console.log('no activeWallet.assets: ', activeWallet);
+      return [];
+    }
+
+    return activeWallet.assets.filter(
+      (asset: IAssetState) =>
+        assets[asset.id as any]?.network === 'both' ||
+        assets[asset.id as any]?.network ===
+          activeNetwork[asset.type === AssetType.Constellation ? KeyringNetwork.Constellation : KeyringNetwork.Ethereum]
     );
   }
 );
 
-const selectAllWallets = createSelector(
-  wallets,
-  (wallets: KeyringWalletState[]) => {
-    return [...wallets];
-  }
-);
+const selectActiveNetworkAssetIds = createSelector(selectActiveNetworkAssets, (assets: IAssetState[]): string[] => {
+  return assets.map((asset) => asset.id);
+});
 
 export default {
   selectAllAccounts,
   selectAllDagAccounts,
   selectAllEthAccounts,
-  selectAllWallets
+  selectAllWallets,
+  selectActiveNetworkAssets,
+  selectActiveNetworkAssetIds,
 };
