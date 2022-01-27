fd0d1aa5974cd97cb8e0750addc44af6eaf1b1b5
Trini <trinile@users.noreply.github.com>
2022-01-27 12:29:39 -0800

    Development/trini/sgw 86/networks migration (#194)
    * add select component
    
    * add Networks scene migration
    
    * add Inter-Sans-serif
    
    * clean up styles and logs in select and network container
    
    * clean up
    
    * clean up

---
+++ source/scenes/settings/Networks/Networks.native.tsx
@@ -0,0 +1,58 @@
+///////////////////////
+// Modules
+///////////////////////
+import React, { FC, useState } from 'react';
+import { View, Text } from 'react-native';
+///////////////////////
+// Components
+///////////////////////
+import Select from 'components/Select';
+
+///////////////////////
+// Styles
+///////////////////////
+import styles from './styles';
+
+///////////////////////
+// Types
+///////////////////////
+import INetworkSettings from './types';
+
+const NetworksComponent: FC<INetworkSettings> = ({ networkOptions }) => {
+  //pass in open state methods so we can not have multiple dropdowns open on the same page
+  const [ openNetwork1, setOpenNetwork1 ] = useState(false);
+  const [ openNetwork2, setOpenNetwork2 ] = useState(false);
+  const onOpenNetwork1 = () => {
+    setOpenNetwork2(false);
+  };
+
+  const onOpenNetwork2 = () => {
+    setOpenNetwork1(false);
+  };
+
+  return (
+    <View style={styles.wrapper}>
+      {networkOptions.map((network, i) => {
+        const openProps = {
+          open: i === 0 ? openNetwork1 : openNetwork2,
+          setOpen: i == 0 ? setOpenNetwork1 : setOpenNetwork2,
+          onOpen: i == 0 ? onOpenNetwork1: onOpenNetwork2
+        };
+
+        return (
+          <View key={network.key} style={network.containerStyle}>
+            <Text style={styles.label}>{network.label}</Text>
+            <Select
+              value={network.value}
+              onChange={network.onChange}
+              options={network.options}
+              extraProps={{...network.extraProps, ...openProps}}
+            ></Select>
+          </View>
+        );
+      })}
+    </View>
+  );
+};
+
+export default NetworksComponent;
