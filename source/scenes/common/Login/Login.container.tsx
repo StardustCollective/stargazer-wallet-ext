b8af5c846eedf435b2edfcc5cf8ffd9f8b05b89e
Cesar Miranda <cesar@Cesars-MacBook-Pro-2.local>
2022-02-04 07:52:00 -0800

    Added login

---
+++ source/scenes/common/Login/Login.container.tsx
@@ -0,0 +1,100 @@
+///////////////////////////
+// Modules
+///////////////////////////
+
+import React, { FC, useState } from 'react';
+
+///////////////////////////
+// Components
+///////////////////////////
+
+import Container from 'components/Container'
+
+//////////////////////
+// Hooks Imports
+///////////////////// 
+
+import { useForm } from 'react-hook-form';
+
+//////////////////////
+// Controllers
+///////////////////// 
+
+import WalletController from 'scripts/Background/controllers/WalletController';
+
+////////////////////////
+// Scene
+/////////////////////// 
+
+import Login from './Login'
+
+//////////////////////
+// Constants
+///////////////////// 
+
+import { schema } from './consts';
+
+////////////////////////
+// Types
+/////////////////////// 
+
+type ILoginProps = {
+  onLoginSuccess: (res: boolean) => void;
+  onLoginError?: () => void;
+  onImportClicked?: () => void;
+}
+
+////////////////////////
+// Container
+/////////////////////// 
+
+const LoginContainer: FC<ILoginProps> = ({
+  onLoginSuccess,
+  onLoginError,
+  onImportClicked,
+}) => {
+
+  const { control, handleSubmit, register, errors } = useForm({
+    validationSchema: schema,
+  });
+  const [isInvalid, setInvalid] = useState(false);
+
+  const onSubmit = (data: any) => {
+    // An unlock response of  false means migration attempt failed but user is logged in
+    WalletController.unLock(data.password).then(async (res: boolean) => {
+      if (onLoginSuccess) {
+        onLoginSuccess(res)
+      }
+      setInvalid(false);
+    })
+      .catch((err: any) => {
+        if (onLoginError) {
+          onLoginError();
+        }
+        setInvalid(true);
+      });
+  };
+
+  const importClicked = () => {
+    if (onImportClicked) {
+      onImportClicked();
+    }
+  }
+
+  return (
+    <Container>
+      <Login
+        control={control}
+        importClicked={importClicked}
+        handleSubmit={handleSubmit}
+        onSubmit={onSubmit}
+        errors={errors}
+        register={register}
+        isInvalid={isInvalid}
+      />
+    </Container>
+  );
+
+}
+
+export default LoginContainer;
---
+++ source/scenes/common/Login/Login.native.tsx
@@ -0,0 +1,103 @@
+///////////////////////////
+// Modules
+///////////////////////////
+
+import React, { FC } from "react";
+import { View } from 'react-native';
+import TextV3, { TEXT_ALIGN_ENUM } from 'components/TextV3';
+import ButtonV3, { BUTTON_TYPES_ENUM, BUTTON_SIZES_ENUM } from 'components/ButtonV3';
+import Link from 'components/Link';
+import TextInput from 'components/TextInput';
+
+///////////////////////////
+// Images
+///////////////////////////
+
+import Logo from 'assets/images/logo';
+
+///////////////////////////
+// Styles
+///////////////////////////
+
+import styles from './styles';
+import { COLORS_ENUMS } from 'assets/styles/colors';
+
+///////////////////////////
+// Constants
+///////////////////////////
+
+const LOGO_IMAGE_SIZE = 192;
+
+// Strings
+const UNLOCK_STRING = 'Unlock';
+const PLEASE_ENTER_YOUR_PASSWORD_STRING = 'Please enter your password';
+const LOGIN_ERROR_STRING = 'Error: Invalid password';
+
+//////////////////////
+// Types
+//////////////////////
+
+import ILogin from './types';
+
+const Login: FC<ILogin> = ({
+  control,
+  importClicked,
+  handleSubmit,
+  onSubmit,
+  errors,
+  register,
+  isInvalid,
+}) => {
+
+
+  return (
+    <View style={styles.layout}>
+      <TextV3.HeaderLarge
+        align={TEXT_ALIGN_ENUM.CENTER}
+      >
+        Welcome to {'\n'} Stargazer Wallet
+      </TextV3.HeaderLarge>
+      <Logo
+        width={LOGO_IMAGE_SIZE}
+        height={LOGO_IMAGE_SIZE}
+        style={styles.logo}
+      />
+      <View style={styles.input}>
+        <TextInput
+          id="createPass-password"
+          type="password"
+          name="password"
+          placeholder={PLEASE_ENTER_YOUR_PASSWORD_STRING}
+          control={control}
+        />
+        {errors.password ? (
+          <TextV3.CaptionStrong color={COLORS_ENUMS.RED}>
+            {errors.password.message}
+          </TextV3.CaptionStrong>
+        ) : (
+          isInvalid && (
+            <TextV3.CaptionStrong color={COLORS_ENUMS.RED}>
+              {LOGIN_ERROR_STRING}
+            </TextV3.CaptionStrong>
+          )
+        )}
+      </View>
+
+      <ButtonV3
+        type={BUTTON_TYPES_ENUM.ACCENT_ONE_SOLID}
+        size={BUTTON_SIZES_ENUM.LARGE}
+        title={UNLOCK_STRING}
+        extraStyle={styles.unlockButton}
+        onPress={handleSubmit(data => { onSubmit(data) })}
+      />
+      <Link
+        color="monotoneOne"
+        onPress={importClicked}
+        title="Reset and restore from recovery seed phrase"
+      />
+    </View>
+  )
+
+}
+
+export default Login;
