db21570440de183604f2d9af8faaf4acb6d547e9
Trini Le <trinile@gmail.com>
2022-02-04 08:09:59 -0800

    add wallet screen

---
+++ source/scenes/settings/AddWallet/AddWallet.native.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+
+import Icon from 'components/Icon';
+import TextV3 from 'components/TextV3';
+import StargazerIcon from 'assets/images/logo-s.svg';
+
+import { COLORS_ENUMS } from 'assets/styles/colors';
+
+import styles from './styles';
+import AddWalletSettings from './types';
+
+const AddWallet: FC<AddWalletSettings> = ({ onCreateNewWalletClicked, onImportWalletClicked }) => {
+  return (
+    <View style={styles.wrapper}>
+      <TouchableOpacity onPress={onCreateNewWalletClicked}>
+        <View testID="addWallet-createNewWallet" style={styles.menu}>
+          <View style={styles.stargazerIconWrapper}>
+            <StargazerIcon width={24} iconStyles={styles.icon} />
+          </View>
+          <TextV3.Body color={COLORS_ENUMS.BLACK} extraStyles={styles.menuText}>
+            Create New Wallet
+          </TextV3.Body>
+          <Icon type="font_awesome" name="chevron-right" iconContainerStyles={styles.iconWrapper} />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onImportWalletClicked}>
+        <View testID="addWallet-importWallet" style={styles.menu}>
+          <View style={styles.stargazerIconWrapper}>
+            <StargazerIcon width={24} iconStyles={styles.icon} />
+          </View>
+          <TextV3.Body color={COLORS_ENUMS.BLACK} extraStyles={styles.menuText}>
+            Import Wallet
+          </TextV3.Body>
+          <Icon type="font_awesome" name="chevron-right" iconContainerStyles={styles.iconWrapper} />
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default AddWallet;
---
+++ source/scenes/settings/AddWallet/styles.ts
@@ -0,0 +1,71 @@
+import { StyleSheet } from 'react-native';
+import { COLORS, FONTS } from 'assets/styles/_variables';
+const styles = StyleSheet.create({
+  wrapper: {
+    width: '100%',
+    padding: 24,
+    flexDirection: 'column',
+    overflow: 'scroll',
+    paddingHorizontal: 24,
+    paddingVertical: 0,
+    backgroundColor: COLORS.gray_light_100,
+    flexGrow: 1,
+  },
+  menu: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingHorizontal: 12,
+    paddingVertical: 0,
+    height: 60,
+    alignItems: 'center',
+    transition: 'all 0.3s',
+    backgroundColor: COLORS.white,
+    borderBottom: `1px solid ${COLORS.gray_light}`,
+    borderRadius: 6,
+    marginBottom: 12,
+  },
+  menuText: {
+    lineHeight: 24,
+    color: COLORS.gray_dark,
+    fontWeight: '500',
+    fontSize: 16,
+    flexGrow: 1,
+    fontFamily: FONTS.rubik,
+  },
+  iconWrapper: {
+    // backgroundColor: COLORS.primary,
+    backgroundColor: 'transparent',
+  },
+  icon: {
+    fontSize: 16,
+    color: COLORS.gray,
+    backgroundColor: 'white',
+  },
+  text: {
+    fontFamily: FONTS.rubik,
+    flexGrow: 1,
+  },
+  stargazerIconWrapper: {
+    backgroundColor: COLORS.primary,
+    marginRight: 12,
+    borderRadius: 18,
+    height: 36,
+    width: 36,
+    position: 'relative',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+// text: {
+//   marginTop: 12,
+//   marginBottom: 24,
+//   fontSize: 12,
+//   fontWeight: '500',
+//   fontFamily: FONTS.rubik,
+//   lineHeight: 16,
+// },
+// iconWrapper: {
+//   backgroundColor: 'transparent',
+// },
+
+export default styles;
