b7d48eca61b7a97d51eacdfd779ecb830a65fbfc
Cesar Miranda <cesar@Cesars-MacBook-Pro-2.local>
2022-02-07 00:22:57 -0800

    Clean up

--- source/scenes/home/Home/AssetsPanel/AssetPanel.container.tsx
+++ source/scenes/home/Home/AssetsPanel/AssetPanel.container.tsx
@@ -1,54 +1,89 @@
-import { RootState } from 'state/store';
+///////////////////////
+// Module
+///////////////////////
+
+import React from 'react';
+
 import { useSelector } from 'react-redux';
 import { useLinkTo } from '@react-navigation/native';
 
-import { useController } from 'hooks/index';
-import walletSelectors from 'selectors/walletsSelectors';
+///////////////////////
+// Components
+///////////////////////
 
 import Container from 'components/Container';
 
+///////////////////////
+// Selectors
+///////////////////////
+
+// import { useController } from 'hooks/index';
+import walletSelectors from 'selectors/walletsSelectors';
+
+///////////////////////
+// Scene
+///////////////////////
+
 import AssetsPanel from './AssetsPanel';
 
+///////////////////////
+// Types
+///////////////////////
+
+import { RootState } from 'state/store';
 import IVaultState, { IAssetState } from 'state/vault/types';
 import { INFTInfoState, INFTListState } from 'state/nfts/types';
 import IAssetListState from 'state/assets/types';
 
-
+///////////////////////
+// Container
+///////////////////////
 
 const AssetsPanelContainer = () => {
-    const controller = useController();
-    const linkTo = useLinkTo();
-    const { activeWallet }: IVaultState = useSelector((state: RootState) => state.vault);
-    const assets: IAssetListState = useSelector((state: RootState) => state.assets);
-    const nfts: INFTListState = useSelector((state: RootState) => state.nfts);
-  
-    const activeNetworkAssets = useSelector(walletSelectors.selectActiveNetworkAssets);
-    const activeNFTAssets = useSelector(walletSelectors.selectNFTAssets);
-  
-    const handleSelectAsset = (asset: IAssetState) => {
-      controller.wallet.account.updateAccountActiveAsset(asset);
-      linkTo('/asset');
-    };
-  
-    const handleSelectNFT = (nft: INFTInfoState) => {
-      window.open(nft.link, '_blank');
-    };
-  
 
+  // const controller = useController();
 
-    return(
-        <Container>
-            <AssetsPanel 
-              activeNetworkAssets={activeNetworkAssets}
-              handleSelectAsset={handleSelectAsset}
-              assets={assets}
-              activeNFTAssets={activeNFTAssets}
-              nfts={nfts}
-              handleSelectNFT={handleSelectNFT}
-              activeWallet={activeWallet}
-            />
-        </Container>
-    );
+  ///////////////////////
+  // Hooks
+  ///////////////////////
+
+  const linkTo = useLinkTo();
+  const { activeWallet }: IVaultState = useSelector((state: RootState) => state.vault);
+  const assets: IAssetListState = useSelector((state: RootState) => state.assets);
+  const nfts: INFTListState = useSelector((state: RootState) => state.nfts);
+  const activeNetworkAssets = useSelector(walletSelectors.selectActiveNetworkAssets);
+  const activeNFTAssets = useSelector(walletSelectors.selectNFTAssets);
+
+  ///////////////////////
+  // Callbacks
+  ///////////////////////
+
+  const handleSelectAsset = (asset: IAssetState) => {
+    // controller.wallet.account.updateAccountActiveAsset(asset);
+    linkTo('/asset');
+  };
+
+  const handleSelectNFT = (nft: INFTInfoState) => {
+    window.open(nft.link, '_blank');
+  };
+
+  ///////////////////////
+  // Render
+  ///////////////////////
+
+  return (
+    <Container>
+      <AssetsPanel
+        activeNetworkAssets={activeNetworkAssets}
+        handleSelectAsset={handleSelectAsset}
+        assets={assets}
+        activeNFTAssets={activeNFTAssets}
+        nfts={nfts}
+        handleSelectNFT={handleSelectNFT}
+        activeWallet={activeWallet}
+      />
+    </Container>
+  );
 
 }
 
