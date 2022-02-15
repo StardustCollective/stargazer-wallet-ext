f5c115c0dec3ae493efa67bcc1f656ae66aabbd7
Cesar Miranda <cesar@Cesars-MacBook-Pro-2.local>
2022-02-14 23:24:20 -0800

    Fixed: Issue where card component would not fully expand do to overflow setting

--- source/scenes/home/Home/AssetsPanel/styles.ts
+++ source/scenes/home/Home/AssetsPanel/styles.ts
@@ -2,15 +2,13 @@ import { StyleSheet } from 'react-native';
 import { COLORS } from 'assets/styles/_variables';
 
 const styles = StyleSheet.create({
-
   activity: {
-    minHeight: '60%',
-    width: "100%",
     backgroundColor: COLORS.white,
     borderTopRightRadius: 8,
     borderTopLeftRadius: 8,
   },
   content: {
+    overflow: 'scroll',
     margin: 16,
   }
 
