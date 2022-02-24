9fc0afb1f7ddf1b4cbcfb32e5135affd1caa7924
Trini Le <trinile@gmail.com>
2022-02-24 10:37:58 -0800

    wip async storage redux-persist

--- source/native/App.tsx
+++ source/native/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import {PersistGate} from 'redux-persist/integration/react';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NativeBaseProvider} from 'native-base';
 import RootStack from 'navigation/stacks/Root';
 import linking from 'navigation/linking';
-import Store from 'state/store';
-import { STORE_PORT, DAG_NETWORK } from 'constants/index';
+import Store, {persistor} from 'state/store';
+import {STORE_PORT, DAG_NETWORK} from 'constants/index';
 import {Provider} from 'react-redux';
 import FlashMessage from 'react-native-flash-message';
-import { dag4 } from '@stardust-collective/dag4';
+import {dag4} from '@stardust-collective/dag4';
 
-import { KeyringNetwork } from '@stardust-collective/dag4-keyring';
+import {KeyringNetwork} from '@stardust-collective/dag4-keyring';
 
 ////////////////////
 // DAG Config
 ////////////////////
 
 const vault = Store.getState().vault;
-const networkId = (vault && vault.activeNetwork && vault.activeNetwork[KeyringNetwork.Constellation]);
+const networkId =
+  vault &&
+  vault.activeNetwork &&
+  vault.activeNetwork[KeyringNetwork.Constellation];
 const networkInfo = (networkId && DAG_NETWORK[networkId]) || DAG_NETWORK.main;
 
 dag4.di.getStateStorageDb().setPrefix('stargazer-');
@@ -26,18 +30,19 @@ dag4.di.useLocalStorageClient(global.localStorage);
 dag4.network.config({
   id: networkInfo.id,
   beUrl: networkInfo.beUrl,
-  lbUrl: networkInfo.lbUrl
+  lbUrl: networkInfo.lbUrl,
 });
-
 
 const App = () => {
   return (
     <SafeAreaProvider>
       <NativeBaseProvider>
         <Provider store={Store}>
+          {/* <PersistGate loading={null} persistor={persistor}> */}
           <NavigationContainer linking={linking}>
             <RootStack />
           </NavigationContainer>
+          {/* </PersistGate> */}
         </Provider>
       </NativeBaseProvider>
       <FlashMessage position="top" />
--- source/state/store.ts
+++ source/state/store.ts
@@ -1,38 +1,37 @@
-import {
-  combineReducers,
-  configureStore,
-  getDefaultMiddleware,
-  Store,
-} from '@reduxjs/toolkit';
+import { combineReducers, configureStore, getDefaultMiddleware, Store } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
 import logger from 'redux-logger';
 import throttle from 'lodash/throttle';
 
-import vault  from './vault';
+import vault from './vault';
 import price from './price';
 import contacts from './contacts';
 import assets from './assets';
 import nfts from './nfts';
 
 import dapp from './dapp';
-import { saveState, loadState } from './localStorage';
+import { saveState } from './localStorage';
 
-const middleware = [
-  ...getDefaultMiddleware({ thunk: false, serializableCheck: false }),
-];
+const middleware = [...getDefaultMiddleware({ thunk: false, serializableCheck: false })];
 
 if (process.env.NODE_ENV !== 'production') {
   middleware.push(logger);
 }
 
-// const preloadedState = loadState();
-
-// //v1.0 wallet state
-// if (preloadedState && preloadedState.wallet) {
-//   delete preloadedState.wallet;
-// }
+// const persistConfig = {
+//   key: 'state',
+//   storage: AsyncStorage,
+// };
 
 const store: Store = configureStore({
   reducer: combineReducers({
+    // vault: persistReducer(persistConfig, vault),
+    // price: persistReducer(persistConfig, price),
+    // contacts: persistReducer(persistConfig, contacts),
+    // assets: persistReducer(persistConfig, assets),
+    // nfts: persistReducer(persistConfig, nfts),
+    // dapp: persistReducer(persistConfig, dapp),
     vault,
     price,
     contacts,
@@ -40,6 +39,7 @@ const store: Store = configureStore({
     nfts,
     dapp,
   }),
+
   middleware,
   devTools: process.env.NODE_ENV !== 'production',
   // preloadedState,
@@ -59,9 +59,12 @@ function updateState() {
 
 store.subscribe(
   throttle(() => {
+    // every second we update store state
     updateState();
   }, 1000)
 );
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
