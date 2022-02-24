d7e46f4f2d2721d5211b6786665680bedb6f957e
Cesar Miranda <cmirandac665@gmail.com>
2022-02-24 02:37:51 -0800

    Migrated: AssetPanel to new controller util lib

--- source/scenes/home/Home/AssetsPanel/AssetsPanel.container.tsx
+++ source/scenes/home/Home/AssetsPanel/AssetsPanel.container.tsx
@@ -17,7 +17,6 @@ import Container from 'components/Container';
 // Selectors
 ///////////////////////
 
-// import { useController } from 'hooks/index';
 import walletSelectors from 'selectors/walletsSelectors';
 
 ///////////////////////
@@ -25,6 +24,12 @@ import walletSelectors from 'selectors/walletsSelectors';
 ///////////////////////
 
 import AssetsPanel from './AssetsPanel';
+
+///////////////////////
+// Utils
+///////////////////////
+
+import { getAccountController } from 'utils/controllersUtils';
 
 ///////////////////////
 // Types
@@ -41,7 +46,7 @@ import IAssetListState from 'state/assets/types';
 
 const AssetsPanelContainer = () => {
 
-  // const controller = useController();
+   const accountController = getAccountController();
 
   ///////////////////////
   // Hooks
@@ -54,15 +59,12 @@ const AssetsPanelContainer = () => {
   const activeNetworkAssets = useSelector(walletSelectors.selectActiveNetworkAssets);
   const activeNFTAssets = useSelector(walletSelectors.selectNFTAssets);
 
-  console.log('!!!!!!!Active Wallet: ');
-  console.log(activeWallet);
-
   ///////////////////////
   // Callbacks
   ///////////////////////
 
   const handleSelectAsset = (asset: IAssetState) => {
-    // controller.wallet.account.updateAccountActiveAsset(asset);
+    accountController.updateAccountActiveAsset(asset);
     linkTo('/asset');
   };
 
