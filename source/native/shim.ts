fb3efa2b1b660623961a9b87b6eedfaeff6365bd
Cesar <84341669+cmirandac665@users.noreply.github.com>
2022-03-09 20:36:27 -0800

    Fixed: Issue where importing constellation json file was taking very long (#244)

--- source/native/shim.ts
+++ source/native/shim.ts
@@ -1,4 +1,8 @@
+import { scrypt } from 'react-native-fast-crypto';
+
+
 global.process = require('process/browser.js');
 global.localStorage = require('@react-native-async-storage/async-storage').default;
 global.crypto = require('react-native-crypto');
 global.location = undefined;
+global.scrypt = scrypt;
