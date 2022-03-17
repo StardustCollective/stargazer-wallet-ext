74de0c820f4dad8ddd58a1cd9b743a3307195c6b
Cesar Miranda <cmirandac665@gmail.com>
2022-03-17 00:53:50 -0700

    Added: Buy DAG button to home screen

--- source/scenes/home/Home/Home.native.tsx
+++ source/scenes/home/Home/Home.native.tsx
@@ -3,7 +3,8 @@
 ///////////////////////////
 
 import React, { FC } from 'react';
-import { View, ActivityIndicator, ScrollView } from 'react-native';
+import { View, ActivityIndicator, ScrollView, Linking } from 'react-native';
+import { useSelector } from 'react-redux';
 
 ///////////////////////////
 // Components
@@ -11,6 +12,8 @@ import { View, ActivityIndicator, ScrollView } from 'react-native';
 
 import AssetsPanel from './AssetsPanel';
 import TextV3 from 'components/TextV3';
+import ButtonV3, { BUTTON_TYPES_ENUM, BUTTON_SIZES_ENUM } from 'components/ButtonV3';
+
 ///////////////////////////
 // Styles
 ///////////////////////////
@@ -22,18 +25,31 @@ import styles from './styles';
 ///////////////////////////
 
 import { IHome } from './types';
+import { RootState } from 'state/store';
+import IVaultState from 'state/vault/types';
+
 ///////////////////////////
 // Constants
 ///////////////////////////
 
 const ACTIVITY_INDICATOR_SIZE = 'large';
 const ACTIVITY_INDICATOR_COLOR = '#FFF';
+const BUY_DAG_URL = 'https://howtobuydag.com/';
 
 ///////////////////////////
 // Scene
 ///////////////////////////
 
 const Home: FC<IHome> = ({ activeWallet, balanceObject, balance }) => {
+
+
+  const { balances }: IVaultState = useSelector((state: RootState) => state.vault);
+  const constellationBalance  = Number(balances.constellation);
+
+  const onHowToBuyDagPressed = () => {
+    Linking.openURL(BUY_DAG_URL);
+  }
+
   return (
     <View style={styles.container}>
       <ScrollView style={styles.scrollView} contentContainerStyle={styles.scrollViewContentContainer}>
@@ -50,6 +66,17 @@ const Home: FC<IHome> = ({ activeWallet, balanceObject, balance }) => {
               <View style={styles.bitcoinBalance}>
                 <TextV3.Body>{`≈ ₿${balance}`}</TextV3.Body>
               </View>
+              {!constellationBalance && (
+                <>
+                  <ButtonV3
+                    title="How to Buy DAG"
+                    size={BUTTON_SIZES_ENUM.LARGE}
+                    type={BUTTON_TYPES_ENUM.ACCENT_ONE_SOLID}
+                    onPress={onHowToBuyDagPressed}
+                    extraStyles={{ marginTop: 20, marginBottom: 10 }}
+                  />
+                </>
+              )}
             </View>
             <AssetsPanel />
           </>
@@ -59,7 +86,7 @@ const Home: FC<IHome> = ({ activeWallet, balanceObject, balance }) => {
           </View>
         )}
       </ScrollView>
-    </View>
+    </View >
   );
 };
 
