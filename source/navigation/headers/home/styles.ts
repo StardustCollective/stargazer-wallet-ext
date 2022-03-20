ab7e236bf0d0b7c849b71392dcf9e713d5d8871e
Cesar Miranda <cmirandac665@gmail.com>
2022-03-19 19:37:33 -0700

    Fixed: formatting

--- source/navigation/headers/home/styles.ts
+++ source/navigation/headers/home/styles.ts
@@ -1,9 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    logo: {
-        marginLeft: 20
-    }
+  logo: {
+    marginLeft: 20
+  }
 });
 
 export default styles;
