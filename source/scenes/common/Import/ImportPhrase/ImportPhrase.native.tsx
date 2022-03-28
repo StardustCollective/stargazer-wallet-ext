9ae148da58ae584e57192b0ea25f3c07202c05ec
Juan Olmedo <juan.olmedo@constellationnetwork.io>
2022-03-28 15:10:26 -0300

    KeyboardAvoidingView added in ImportPhrase scene

--- source/scenes/common/Import/ImportPhrase/ImportPhrase.native.tsx
+++ source/scenes/common/Import/ImportPhrase/ImportPhrase.native.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { View } from 'react-native';
+import { View, KeyboardAvoidingView } from 'react-native';
 import Layout from 'scenes/common/Layout';
 import TextV3 from 'components/TextV3';
 import TextInput from 'components/TextInput';
@@ -23,40 +23,41 @@ const ImportPhrase: FC<IImportPhrase> = ({
 }) => {
 
   return (
-    <Layout title="Let's import your wallet">
-      <TextV3.Caption color={COLORS_ENUMS.BLACK}>
-        Paste your recovery seed phrase below:
-      </TextV3.Caption>
-      <TextInput
-        name="phrase"
-        type="text"
-        autoCapitalize="none"
-        returnKeyType="done"
-        autoCorrect={false} 
-        control={control}
-        keyboardType='web-search'
-        inputContainerStyle={styles.phraseInput}
-        multiline
-        fullWidth
-        blurOnSubmit
-      />
-      <TextV3.CaptionStrong color={COLORS_ENUMS.BLACK}>
-        The phrase can be 12, 15, 18, 21 or 24 words with a single space between.
-      </TextV3.CaptionStrong>
-      {isInvalid && (
-        <TextV3.Caption extraStyles={styles.errorLabel}color={COLORS_ENUMS.RED}>Invalid recovery seed phrase</TextV3.Caption>
-      )}
-      <View style={styles.buttonContainer}>
-        <ButtonV3
-          type={BUTTON_TYPES_ENUM.PRIMARY}
-          size={BUTTON_SIZES_ENUM.LARGE}
-          title={'Import'}
-          disabled={isDisabled}
-          onPress={handleSubmit(data => { onSubmit(data) })}
+    <KeyboardAvoidingView style={styles.container} behavior='padding'>
+      <Layout title="Let's import your wallet">
+        <TextV3.Caption color={COLORS_ENUMS.BLACK}>
+          Paste your recovery seed phrase below:
+        </TextV3.Caption>
+        <TextInput
+          name="phrase"
+          type="text"
+          autoCapitalize="none"
+          returnKeyType="done"
+          autoCorrect={false} 
+          control={control}
+          keyboardType='web-search'
+          inputContainerStyle={styles.phraseInput}
+          multiline
+          fullWidth
+          blurOnSubmit
         />
-      </View>
-
-    </Layout>
+        <TextV3.CaptionStrong color={COLORS_ENUMS.BLACK}>
+          The phrase can be 12, 15, 18, 21 or 24 words with a single space between.
+        </TextV3.CaptionStrong>
+        {isInvalid && (
+          <TextV3.Caption extraStyles={styles.errorLabel}color={COLORS_ENUMS.RED}>Invalid recovery seed phrase</TextV3.Caption>
+        )}
+        <View style={styles.buttonContainer}>
+          <ButtonV3
+            type={BUTTON_TYPES_ENUM.PRIMARY}
+            size={BUTTON_SIZES_ENUM.LARGE}
+            title={'Import'}
+            disabled={isDisabled}
+            onPress={handleSubmit(data => { onSubmit(data) })}
+          />
+        </View>
+      </Layout>
+    </KeyboardAvoidingView>
   );
 };
 
