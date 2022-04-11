a31df74b0b867f772c504643773a6b52880af58f
Juan Olmedo <juan.olmedo@constellationnetwork.io>
2022-04-11 15:12:13 -0300

    Sentry setup done

--- source/native/index.js
+++ source/native/index.js
@@ -7,5 +7,13 @@ import "@ethersproject/shims"
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import * as Sentry from "@sentry/react-native";
 
-AppRegistry.registerComponent(appName, () => App);
+Sentry.init({
+  dsn: process.env.SENTRY_DNS,
+  tracesSampleRate: __DEV__ ? 1 : 0.2,
+  debug: __DEV__ ? true : false,
+  environment: __DEV__ ? 'development' : 'production'
+});
+
+AppRegistry.registerComponent(appName, () => Sentry.wrap(App));
