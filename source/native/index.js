fd761de4359b808af984698e6a44cc002f65e05a
Juan Olmedo <juan.olmedo@constellationnetwork.io>
2022-04-11 18:31:51 -0300

    maxFontSizeMultiplier for

--- source/native/index.js
+++ source/native/index.js
@@ -4,8 +4,14 @@
 
 import './shim';
 import "@ethersproject/shims"
-import {AppRegistry} from 'react-native';
+import { AppRegistry , Text, TextInput} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+Text.defaultProps = {};
+Text.defaultProps.maxFontSizeMultiplier = 1.2;
+
+TextInput.defaultProps = {};
+TextInput.defaultProps.maxFontSizeMultiplier = 1.2;
 
 AppRegistry.registerComponent(appName, () => App);
