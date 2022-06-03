a447a9d75800e619003e131e62460a409b476835
Juan David Suarez <juandavidkincaid@gmail.com>
2022-06-03 09:50:11 -0500

    removed unnecessary changes

--- source/native/index.js
+++ source/native/index.js
@@ -9,7 +9,6 @@ import {AppRegistry, Text, TextInput} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import * as Sentry from '@sentry/react-native';
-import 'react-native-url-polyfill';
 
 Text.defaultProps = {};
 Text.defaultProps.maxFontSizeMultiplier = 1.2;
