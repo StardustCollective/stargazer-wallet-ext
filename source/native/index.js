9a9a68b3858f6cfeb91ac3ae0f3f9bcd89a921bb
Juan David Suarez <juandavidkincaid@gmail.com>
2022-06-03 08:53:51 -0500

    fixed params encoding bug

--- source/native/index.js
+++ source/native/index.js
@@ -4,11 +4,12 @@
 
 import './shim';
 import 'fast-text-encoding';
-import "@ethersproject/shims"
-import { AppRegistry , Text, TextInput} from 'react-native';
+import '@ethersproject/shims';
+import {AppRegistry, Text, TextInput} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import * as Sentry from "@sentry/react-native";
+import * as Sentry from '@sentry/react-native';
+import 'react-native-url-polyfill';
 
 Text.defaultProps = {};
 Text.defaultProps.maxFontSizeMultiplier = 1.2;
