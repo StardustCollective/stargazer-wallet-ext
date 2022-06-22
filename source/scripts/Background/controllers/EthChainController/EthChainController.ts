17bcdf3b83d6ac4bb1ad20462361b563fb450dc2
Juan David Suarez <juandavidkincaid@gmail.com>
2022-06-22 14:22:40 -0500

    fix some details related to active assets

--- source/scripts/Background/controllers/EthChainController/EthChainController.ts
+++ source/scripts/Background/controllers/EthChainController/EthChainController.ts
@@ -5,30 +5,34 @@ import { EtherscanProvider, getDefaultProvider } from '@ethersproject/providers'
 import { Provider, TransactionResponse } from '@ethersproject/abstract-provider';
 import { tokenContractHelper } from 'scripts/Background/helpers/tokenContractHelper';
 import { parseUnits, toUtf8Bytes } from 'ethers/lib/utils';
-import { 
-  BASE_TOKEN_GAS_COST, 
-  ETHAddress, 
-  ETH_DECIMAL, 
-  InfuraProvider, 
-  SIMPLE_GAS_COST 
+import {
+  BASE_TOKEN_GAS_COST,
+  ETHAddress,
+  ETH_DECIMAL,
+  InfuraProvider,
+  SIMPLE_GAS_COST,
 } from './constants';
-import { 
+import {
   getDefaultGasPrices,
   getNetworkInfo,
   getTokenAddress,
-  validateAddress 
+  validateAddress,
 } from './utils';
-import { 
+import {
   EthChainControllerParams,
   FeesParamsEth,
-  IEthChainController, 
-  InfuraCreds, 
-  EthereumNetwork, 
+  IEthChainController,
+  InfuraCreds,
+  EthereumNetwork,
   TxOverrides,
-  EthNetworkId
+  EthNetworkId,
 } from './types';
 import { Address, FeeOptionKey, TxHistoryParams, TxParams } from '../ChainsController';
-import { getETHTransactionHistory, getGasOracle, getTokenTransactionHistory } from './etherscanApi';
+import {
+  getETHTransactionHistory,
+  getGasOracle,
+  getTokenTransactionHistory,
+} from './etherscanApi';
 import { GasOracleResponse } from './etherscanApi.types';
 import erc20abi from 'utils/erc20.json';
 
@@ -42,7 +46,12 @@ class EthChainController implements IEthChainController {
   private infuraCreds: InfuraCreds | null = null;
   private infuraProjectId: string;
 
-  constructor({ network, privateKey, etherscanApiKey, infuraCreds }: EthChainControllerParams) {
+  constructor({
+    network,
+    privateKey,
+    etherscanApiKey,
+    infuraCreds,
+  }: EthChainControllerParams) {
     this.network = getNetworkInfo(network);
     this.etherscanApiKey = etherscanApiKey;
     this.etherscan = new EtherscanProvider(this.network.value, this.etherscanApiKey);
@@ -92,7 +101,7 @@ class EthChainController implements IEthChainController {
 
   getExplorerUrl = (): string => {
     return this.network.etherscan;
-  }
+  };
 
   setNetwork(network: EthNetworkId) {
     if (!network) {
@@ -106,7 +115,10 @@ class EthChainController implements IEthChainController {
         }
         this.provider = this.infuraCreds.projectSecret
           ? new ethers.providers.InfuraProvider(this.network.value, this.infuraCreds)
-          : new ethers.providers.InfuraProvider(this.network.value, this.infuraCreds.projectId);
+          : new ethers.providers.InfuraProvider(
+              this.network.value,
+              this.infuraCreds.projectId
+            );
       } else {
         this.provider = getDefaultProvider(this.network.value);
       }
@@ -151,34 +163,42 @@ class EthChainController implements IEthChainController {
 
       // feeOptionKey
 
-      const defaultGasLimit: BigNumber = isETHAddress ? SIMPLE_GAS_COST : BASE_TOKEN_GAS_COST;
+      const defaultGasLimit: BigNumber = isETHAddress
+        ? SIMPLE_GAS_COST
+        : BASE_TOKEN_GAS_COST;
 
       let overrides: TxOverrides = {
         gasLimit: gasLimit || defaultGasLimit,
         gasPrice: gasPrice && BigNumber.from(gasPrice.amount().toFixed()),
-      }
+      };
 
       // override `overrides` if `feeOptionKey` is provided
       if (feeOptionKey) {
         const gasPrice = await this.estimateGasPrices()
           .then((prices) => prices[feeOptionKey])
           .catch(() => getDefaultGasPrices()[feeOptionKey]);
-        const gasLimit = await this.estimateGasLimit({ asset, recipient, amount, memo }).catch(() => defaultGasLimit);
+        const gasLimit = await this.estimateGasLimit({
+          asset,
+          recipient,
+          amount,
+          memo,
+        }).catch(() => defaultGasLimit);
 
         overrides = {
           gasLimit,
           gasPrice: BigNumber.from(gasPrice.amount().toFixed()),
-        }
+        };
       }
 
       let txResult;
       if (assetAddress && !isETHAddress) {
         // Transfer ERC20
-        txResult = await this.call<TransactionResponse>(assetAddress, ERC_20_ABI, 'transfer', [
-          recipient,
-          txAmount,
-          Object.assign({}, { ...overrides, nonce }),
-        ])
+        txResult = await this.call<TransactionResponse>(
+          assetAddress,
+          ERC_20_ABI,
+          'transfer',
+          [recipient, txAmount, Object.assign({}, { ...overrides, nonce })]
+        );
       } else {
         // Transfer ETH
         const transactionRequest = Object.assign(
@@ -186,15 +206,15 @@ class EthChainController implements IEthChainController {
           {
             ...overrides,
             data: memo ? toUtf8Bytes(memo) : undefined,
-          },
-        )
+          }
+        );
 
         txResult = await this.getWallet().sendTransaction(transactionRequest);
       }
 
       return txResult;
     } catch (error) {
-      return Promise.reject(error)
+      return Promise.reject(error);
     }
   }
 
@@ -203,8 +223,7 @@ class EthChainController implements IEthChainController {
       const infuraProvider = new InfuraProvider(chainId, this.infuraProjectId);
       try {
         return tokenContractHelper.getTokenInfo(infuraProvider, address);
-      }
-      catch(e) {
+      } catch (e) {
         console.log('ERROR: getTokenInfo = ', e);
         return null;
       }
@@ -253,47 +272,65 @@ class EthChainController implements IEthChainController {
   }
 
   async estimateGasPrices() {
-    if(this.network.value !== 'homestead'){
+    if (this.network.value !== 'homestead') {
       // Etherscan gas oracle is not working in testnets
       const oneGwei = ethers.BigNumber.from(1e9);
-      const feeData = await this.provider.getFeeData()
-      return{
+      const feeData = await this.provider.getFeeData();
+      return {
         average: baseAmount(feeData.gasPrice.toString(), ETH_DECIMAL),
         fast: baseAmount(feeData.gasPrice.add(oneGwei.mul(5)).toString(), ETH_DECIMAL),
-        fastest: baseAmount(feeData.gasPrice.add(oneGwei.mul(10)).toString(), ETH_DECIMAL),
-      }
+        fastest: baseAmount(
+          feeData.gasPrice.add(oneGwei.mul(10)).toString(),
+          ETH_DECIMAL
+        ),
+      };
     }
 
     try {
-      const response: GasOracleResponse = await getGasOracle(this.etherscan.baseUrl, this.etherscan.apiKey)
+      const response: GasOracleResponse = await getGasOracle(
+        this.etherscan.baseUrl,
+        this.etherscan.apiKey
+      );
 
       // Convert result of gas prices: `Gwei` -> `Wei`
-      const averageWei = parseUnits(response.SafeGasPrice, 'gwei')
-      const fastWei = parseUnits(response.ProposeGasPrice, 'gwei')
-      const fastestWei = parseUnits(response.FastGasPrice, 'gwei')
+      const averageWei = parseUnits(response.SafeGasPrice, 'gwei');
+      const fastWei = parseUnits(response.ProposeGasPrice, 'gwei');
+      const fastestWei = parseUnits(response.FastGasPrice, 'gwei');
 
       return {
         average: baseAmount(averageWei.toString(), ETH_DECIMAL),
         fast: baseAmount(fastWei.toString(), ETH_DECIMAL),
         fastest: baseAmount(fastestWei.toString(), ETH_DECIMAL),
-      }
+      };
     } catch (error) {
       throw new Error(`Failed to estimate gas price: ${error}`);
     }
   }
 
-  async waitForTransaction (hash: string, chainId = 1) {
+  async waitForTransaction(hash: string, chainId = 1) {
     const infuraProvider = new InfuraProvider(chainId, this.infuraProjectId);
     return infuraProvider.waitForTransaction(hash);
   }
 
-  async estimateTokenTransferGasLimit(recipient: string, contractAddress: string, txAmount: BigNumber, defaultValue?: number) {
+  async estimateTokenTransferGasLimit(
+    recipient: string,
+    contractAddress: string,
+    txAmount: BigNumber,
+    defaultValue?: number
+  ) {
     try {
-      const contract = new ethers.Contract(contractAddress, ERC_20_ABI, this.getProvider());
-      const gasLimit: BigNumber = await contract.estimateGas.transfer(recipient, txAmount, { from: this.getAddress() });
+      const contract = new ethers.Contract(
+        contractAddress,
+        ERC_20_ABI,
+        this.getProvider()
+      );
+      const gasLimit: BigNumber = await contract.estimateGas.transfer(
+        recipient,
+        txAmount,
+        { from: this.getAddress() }
+      );
       return gasLimit.toNumber();
-    }
-    catch(e) {
+    } catch (e) {
       return defaultValue;
     }
   }
@@ -313,24 +350,29 @@ class EthChainController implements IEthChainController {
     return ethers.utils.isAddress(address);
   }
 
-  private async estimateGasLimit({ asset, recipient, amount, memo }: FeesParamsEth): Promise<BigNumber> {
+  private async estimateGasLimit({
+    asset,
+    recipient,
+    amount,
+    memo,
+  }: FeesParamsEth): Promise<BigNumber> {
     try {
-      const txAmount = BigNumber.from(amount.amount().toFixed())
+      const txAmount = BigNumber.from(amount.amount().toFixed());
 
-      let assetAddress
+      let assetAddress;
       if (asset && assetToString(asset) !== assetToString(AssetETH)) {
-        assetAddress = getTokenAddress(asset)
+        assetAddress = getTokenAddress(asset);
       }
 
-      let estimate
+      let estimate;
 
       if (assetAddress && assetAddress !== ETHAddress) {
         // ERC20 gas estimate
-        const contract = new ethers.Contract(assetAddress, ERC_20_ABI, this.provider)
+        const contract = new ethers.Contract(assetAddress, ERC_20_ABI, this.provider);
 
         estimate = await contract.estimateGas.transfer(recipient, txAmount, {
           from: this.getAddress(),
-        })
+        });
       } else {
         // ETH gas estimate
         const transactionRequest = {
@@ -338,22 +380,29 @@ class EthChainController implements IEthChainController {
           to: recipient,
           value: txAmount,
           data: memo ? toUtf8Bytes(memo) : undefined,
-        }
+        };
 
-        estimate = await this.provider.estimateGas(transactionRequest)
+        estimate = await this.provider.estimateGas(transactionRequest);
       }
 
       return estimate;
     } catch (error) {
-      return Promise.reject(new Error(`Failed to estimate gas limit: ${error}`))
+      return Promise.reject(new Error(`Failed to estimate gas limit: ${error}`));
     }
   }
 
-  private async call<T>(address: Address, abi: ethers.ContractInterface, func: string, params: Array<any>): Promise<T> {
+  private async call<T>(
+    address: Address,
+    abi: ethers.ContractInterface,
+    func: string,
+    params: Array<any>
+  ): Promise<T> {
     if (!address) {
       return Promise.reject(new Error('address must be provided'));
     }
-    const contract = new ethers.Contract(address, abi, this.provider).connect(this.getWallet());
+    const contract = new ethers.Contract(address, abi, this.provider).connect(
+      this.getWallet()
+    );
     return contract[func](...params);
   }
 }
