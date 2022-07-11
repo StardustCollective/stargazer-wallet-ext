20284d36e8bcc987c4429036fbebe6ac362929ea
Juan Olmedo <juan.olmedo@constellationnetwork.io>
2022-07-11 10:51:13 -0300

    AddCustomAsset types updated

--- source/scenes/home/Asset/AddCustomAsset/types.ts
+++ source/scenes/home/Asset/AddCustomAsset/types.ts
@@ -1,18 +1,20 @@
+import { Control, FieldError, FieldValues, NestDataObject, OnSubmit } from "react-hook-form";
+import { ICustomAssetForm } from "state/erc20assets/types";
 
 export default interface IAddCustomAsset {
-  control?: any;
+  control?: Control<FieldValues>;
   register: any;
-  tokenAddress: any;
-  tokenName: any;
-  tokenSymbol: any;
-  tokenDecimals: any;
-  handleAddressScan?: any;
-  handleAddressChange: any;
-  handleNameChange: any;
-  handleSymbolChange: any;
-  handleDecimalsChange: any;
-  handleSubmit: any;
-  onSubmit: any;
-  errors: any;
+  tokenAddress: string;
+  tokenName: string;
+  tokenSymbol: string;
+  tokenDecimals: string;
+  handleAddressScan?: (value: string) => Promise<void>;
+  handleAddressChange: (value: string) => Promise<void>;
+  handleNameChange: (value: string) => void;
+  handleSymbolChange: (value: string) => void;
+  handleDecimalsChange: (value: string) => void;
+  handleSubmit: (callback: OnSubmit<FieldValues>) => (e?: React.BaseSyntheticEvent<object, any, any>) => Promise<void>;
+  onSubmit: (asset: ICustomAssetForm) => Promise<void>;
+  errors: NestDataObject<FieldValues, FieldError>;
   buttonDisabled: boolean;
 }
