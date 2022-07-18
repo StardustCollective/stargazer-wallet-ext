4dabdb6027662c4f9b3281b89de97e62c9e80bce
Juan David Suárez <64592107+juandavidkincaid@users.noreply.github.com>
2022-07-18 10:45:03 -0500

    Added EIP-712 support (#342)
    * documentation related changes
    
    * implemented eip-712
    
    * multiple changes
    + renamed SignatureConsent by TypedSignatureRequest
    + added support for data JSON strings
    + fixed wrong account comparation
    + added eth_signTypedData_v4 as alias

---
+++ source/scenes/external/TypedSignatureRequest/index.tsx
@@ -0,0 +1,116 @@
+//////////////////////
+// Modules Imports
+/////////////////////
+
+import React from 'react';
+import queryString from 'query-string';
+import { browser } from 'webextension-polyfill-ts';
+
+//////////////////////
+// Common Layouts
+/////////////////////
+
+import CardLayout from 'scenes/external/Layouts/CardLayout'
+
+///////////////////////////
+// Styles
+///////////////////////////
+
+import styles from './index.module.scss';
+
+import { TypedSignatureRequest } from './types';
+
+//////////////////////
+// Component
+/////////////////////
+
+const TypedSignatureRequestScreen = () => {
+  //////////////////////
+  // Hooks
+  /////////////////////
+
+
+  const { data: stringData } = queryString.parse(location.search);
+
+  const { signatureConsent: signatureRequest }:
+    { signatureConsent: TypedSignatureRequest } = JSON.parse(stringData as string);
+
+
+  //////////////////////
+  // Callbacks
+  /////////////////////
+
+  const onNegativeButtonClick = async () => {
+    const background = await browser.runtime.getBackgroundPage();
+    const { windowId } = queryString.parse(window.location.search);
+    const denied = new CustomEvent('signTypedMessageResult', {
+      detail: { windowId, result: false }
+    });
+
+    background.dispatchEvent(denied);
+    window.close();
+  };
+
+  const onPositiveButtonClick = async () => {
+    const background = await browser.runtime.getBackgroundPage();
+
+    const { windowId } = queryString.parse(window.location.search);
+
+    const approved = new CustomEvent('signTypedMessageResult', {
+      detail: { windowId, result: true }
+    });
+
+    background.dispatchEvent(approved);
+    window.close();
+  };
+
+  //////////////////////
+  // Renders
+  /////////////////////
+
+  return (
+    <CardLayout
+      stepLabel={``}
+      originDescriptionLabel={'Requested by:'}
+      headerLabel={'Signature Request'}
+      footerLabel={'Signed messages do not incur gas fees.\nOnly sign messages on sites you trust.'}
+      captionLabel={''}
+      negativeButtonLabel={'Reject'}
+      onNegativeButtonClick={onNegativeButtonClick}
+      positiveButtonLabel={'Sign'}
+      onPositiveButtonClick={onPositiveButtonClick}
+    >
+      <div className={styles.content}>
+        <section>
+          <label>
+            Account
+          </label>
+          <div>
+            {signatureRequest.signer}
+          </div>
+        </section>
+        <section className={styles.message}>
+          <label>
+            Message
+          </label>
+          <div>
+            {signatureRequest.content}
+          </div>
+        </section>
+        {signatureRequest.data && Object.keys(signatureRequest.data).length > 0 && <section className={styles.metadata}>
+          <label>
+            Metadata
+          </label>
+          <div>
+            {Object.entries(signatureRequest.data).map(
+              ([key, value]) => (<small>{key} = {value}</small>)
+            )}
+          </div>
+        </section>}
+      </div>
+    </CardLayout>
+  );
+};
+
+export * from './types'
+export default TypedSignatureRequestScreen;
