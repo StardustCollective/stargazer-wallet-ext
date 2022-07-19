745223cca77011982684d57aa1f6977c1f8ceb20
Juan David Suarez <juandavidkincaid@gmail.com>
2022-07-19 13:50:13 -0500

    fixed dag4.js getBalance() case

--- source/utils/dagUtils.ts
+++ source/utils/dagUtils.ts
@@ -1,11 +1,8 @@
 import { dag4 } from '@stardust-collective/dag4';
 
-export const getAccountByAddress = async (
-  address: string
-) => {
-
+export const getAccountByAddress = async (address: string) => {
   dag4.account.setKeysAndAddress('', '', address);
-  const balance = await dag4.account.getBalance();
+  const balance = (await dag4.account.getBalance()) ?? 0;
   const transactions = await dag4.account.getTransactions(10);
 
   return {
