83889074db18953cf219a1ee4e5590ca964a6b46
Juan Olmedo <juan.olmedo@constellationnetwork.io>
2022-09-21 15:42:32 -0300

    Notice modal on app start

--- source/scenes/home/Home/styles.ts
+++ source/scenes/home/Home/styles.ts
@@ -45,6 +45,34 @@ const styles = StyleSheet.create({
   buyButton: {
     marginVertical: 32,
   },
+  // TODO-421: Remove when Mainnet 2.0 is available 
+  centeredView: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+  },
+  modalView: {
+    margin: 16,
+    marginTop: 130,
+    backgroundColor: "white",
+    borderRadius: 8,
+    paddingHorizontal: 30,
+    paddingVertical: 24,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  modalText: {
+    marginTop: 12,
+    marginBottom: 32,
+  }
 });
 
 export default styles;
