f13f7d4f546f748b48ec6a9358fcc6aa5694dcb9
Cesar Miranda <cmirandac665@gmail.com>
2022-09-23 13:09:40 -0700

    Added: Navigations routes to swapTokens container

--- source/scenes/swap/swapTokens/swapTokens.container.tsx
+++ source/scenes/swap/swapTokens/swapTokens.container.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState, FC } from 'react';
-
 ///////////////////////////
 // Imports
 ///////////////////////////
 
+import React, { useEffect, useState, FC } from 'react';
+import { useSelector } from 'react-redux';
 import { useLinkTo } from '@react-navigation/native';
 
 ///////////////////////////
 // Types
 ///////////////////////////
 
-import {
-  ISwapTokensContainer
-} from './types';
+import { ISwapTokensContainer } from './types';
+import { ISelectedCurrency } from 'state/swapping/types';
+import { RootState } from 'state/store';
 
 ///////////////////////////
 // Components
@@ -25,17 +25,40 @@ import Container from 'components/Container';
 // Constants
 ///////////////////////////
 
+import { SWAP_FROM_ACTION, SWAP_TO_ACTION } from 'scenes/swap/constants';
+const SELECT_CURRENCY_ROUTE = '/tokenList?action=';
+const NEXT_SCREEN_ROUTE = '/transferInfo';
+
+///////////////////////////
+// Container
+///////////////////////////
+
 const SwapTokenContainer: FC<ISwapTokensContainer> = ({navigation, route}) => {
   
   const linkTo = useLinkTo();
+  const { swapTo, swapFrom }: {swapTo: ISelectedCurrency, swapFrom: ISelectedCurrency} = useSelector((state: RootState) => state.swapping);
 
   const onNextPressed = () => {
-    linkTo('/transferInfo');
+    linkTo(NEXT_SCREEN_ROUTE);
+  }
+
+  const onSwapFromTokenListPressed = () => {
+    linkTo(`${SELECT_CURRENCY_ROUTE}${SWAP_FROM_ACTION}`);
+  }
+
+  const onSwapToTokenListPressed = () => {
+    linkTo(`${SELECT_CURRENCY_ROUTE}${SWAP_TO_ACTION}`);
   }
 
   return (
     <Container>
-      <SwapTokens onNextPressed={onNextPressed} />
+      <SwapTokens
+        selectedCurrencySwapFrom={swapFrom.currency}
+        selectedCurrencySwapTo={swapTo.currency}
+        onSwapFromTokenListPressed={onSwapFromTokenListPressed}
+        onSwapToTokenListPressed={onSwapToTokenListPressed}
+        onNextPressed={onNextPressed} 
+      />
     </Container>
   );
 };
