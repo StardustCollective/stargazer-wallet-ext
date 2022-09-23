0a4c17d359df7e19786c45164d2e10ea1b36dc76
Juan Olmedo <juan.olmedo@constellationnetwork.io>
2022-09-23 19:38:04 -0300

    BigInt shim removed for Mobile

--- source/native/shim.ts
+++ source/native/shim.ts
@@ -6,19 +6,3 @@ global.localStorage = require('@react-native-async-storage/async-storage').defau
 global.crypto = require('react-native-crypto');
 global.location = undefined;
 global.scrypt = scrypt;
-// Reference: https://github.com/vaxxnz/nzcp-js/issues/2#issuecomment-972808289
-if (typeof BigInt === 'undefined') {
-    const bi = require('big-integer');
-  
-    function myBigInt(value: any) {
-        if (typeof value === 'string') {
-            const match = value.match(/^0([xo])([0-9a-f]+)$/i)
-            if (match) {
-                return bi(match[2], match[1].toLowerCase() === 'x' ? 16 : 8)
-            }
-        }
-        return bi(value)
-    }
-  
-    global.BigInt = myBigInt
-  }
