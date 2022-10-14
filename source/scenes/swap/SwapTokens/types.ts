e68d4d42339fb020a29d83981e8badc02fb672b5
Cesar Miranda <cmirandac665@gmail.com>
2022-10-14 15:34:09 -0700

    Fixed: Folder name casing for swapTokens to SwapTokens

---
+++ source/scenes/swap/SwapTokens/types.ts
@@ -0,0 +1,24 @@
+
+import { ISearchCurrency, ICurrencyRate } from 'state/swap/types';
+
+export interface ISwapTokensContainer {
+  navigation: any;
+  route: any;
+}
+
+export default interface ISwapTokens {
+  selectedCurrencySwapFrom: ISearchCurrency;
+  selectedCurrencySwapTo: ISearchCurrency;
+  onNextPressed: () => void;
+  onSwapFromTokenListPressed: () => void;
+  onSwapToTokenListPressed: () => void;
+  fromBalance: string;
+  onFromChangeText: ( text: string) => void;
+  isBalanceError: boolean;
+  isNextButtonDisabled: boolean;
+  isRateError: boolean;
+  isCurrencyRateLoading: boolean;
+  currencyRate: ICurrencyRate;
+  toAmount: number;
+  isNextButtonLoading: boolean;
+}
