dfdec350f5280f36c1b756392c690a80d34bd947
Cesar Miranda <cmirandac665@gmail.com>
2022-10-18 01:01:26 -0700

    Revert

--- source/scenes/swap/SwapTokens/SwapTokens.container.tsx
+++ source/scenes/swap/SwapTokens/SwapTokens.container.tsx
@@ -102,7 +102,7 @@ const SwapTokenContainer: FC<ISwapTokensContainer> = ({ navigation }) => {
         walletController.swap.clearSwapFromBalance();
       }
     }
-  }, [supportedAssets.length])
+  }, [supportedAssets])
 
   // Update the active asset when the swapFrom state changes
   useEffect(() => {
