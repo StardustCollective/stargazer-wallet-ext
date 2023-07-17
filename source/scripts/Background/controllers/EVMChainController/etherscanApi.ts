6b2bef5327a50c6b5d5c6f0ba797ca433f48b49f
Juan Olmedo <juan.olmedo@constellationnetwork.io>
2023-07-17 16:38:59 -0300

    Added: internal transactions

--- source/scripts/Background/controllers/EVMChainController/etherscanApi.ts
+++ source/scripts/Background/controllers/EVMChainController/etherscanApi.ts
@@ -1,25 +1,29 @@
 import { bn } from '@xchainjs/xchain-util/lib';
 import { Txs } from '../ChainsController';
-import { 
-  ETHTransactionInfo, 
-  GasOracleResponse, 
-  TokenTransactionInfo, 
-  TransactionHistoryParam 
+import {
+  ETHTransactionInfo,
+  GasOracleResponse,
+  TokenTransactionInfo,
+  TransactionHistoryParam,
 } from './etherscanApi.types';
-import { 
-  filterSelfTxs, 
-  getTxFromEthTransaction, 
-  getTxFromTokenTransaction 
+import {
+  filterSelfTxs,
+  getTxFromEthTransaction,
+  getTxFromTokenTransaction,
 } from './utils';
 
-const getApiKeyQueryParameter = (apiKey?: string): string => (!!apiKey ? `&apiKey=${apiKey}` : '')
+const getApiKeyQueryParameter = (apiKey?: string): string =>
+  !!apiKey ? `&apiKey=${apiKey}` : '';
 
-export const getGasOracle = async (baseUrl: string, apiKey?: string): Promise<GasOracleResponse> => {
+export const getGasOracle = async (
+  baseUrl: string,
+  apiKey?: string
+): Promise<GasOracleResponse> => {
   const url = baseUrl + '/api?module=gastracker&action=gasoracle';
 
   const responseJson = await (await fetch(url + getApiKeyQueryParameter(apiKey))).json();
   return responseJson.result;
-}
+};
 
 export const getTokenTransactionHistory = async ({
   baseUrl,
@@ -31,24 +35,27 @@ export const getTokenTransactionHistory = async ({
   endblock,
   apiKey,
 }: TransactionHistoryParam & { baseUrl: string; apiKey?: string }): Promise<Txs> => {
-  let url = baseUrl + `/api?module=account&action=tokentx&sort=desc` + getApiKeyQueryParameter(apiKey)
-  if (address) url += `&address=${address}`
-  if (assetAddress) url += `&contractaddress=${assetAddress}`
-  if (offset) url += `&offset=${offset}`
-  if (page) url += `&page=${page}`
-  if (startblock) url += `&startblock=${startblock}`
-  if (endblock) url += `&endblock=${endblock}`
- 
+  let url =
+    baseUrl +
+    `/api?module=account&action=tokentx&sort=desc` +
+    getApiKeyQueryParameter(apiKey);
+  if (address) url += `&address=${address}`;
+  if (assetAddress) url += `&contractaddress=${assetAddress}`;
+  if (offset) url += `&offset=${offset}`;
+  if (page) url += `&page=${page}`;
+  if (startblock) url += `&startblock=${startblock}`;
+  if (endblock) url += `&endblock=${endblock}`;
+
   const responseJson = await (await fetch(url)).json();
   const tokenTransactions: TokenTransactionInfo[] = responseJson.result;
 
   return filterSelfTxs(tokenTransactions)
     .filter((tx) => !bn(tx.value).isZero())
     .reduce((acc, cur) => {
-      const tx = getTxFromTokenTransaction(cur)
-      return tx ? [...acc, tx] : acc
-    }, [] as Txs)
-}
+      const tx = getTxFromTokenTransaction(cur);
+      return tx ? [...acc, tx] : acc;
+    }, [] as Txs);
+};
 
 export const getETHTransactionHistory = async ({
   baseUrl,
@@ -59,17 +66,31 @@ export const getETHTransactionHistory = async ({
   endblock,
   apiKey,
 }: TransactionHistoryParam & { baseUrl: string; apiKey?: string }): Promise<Txs> => {
-  let url = baseUrl + `/api?module=account&action=txlist&sort=desc` + getApiKeyQueryParameter(apiKey)
-  if (address) url += `&address=${address}`
-  if (offset) url += `&offset=${offset}`
-  if (page) url += `&page=${page}`
-  if (startblock) url += `&startblock=${startblock}`
-  if (endblock) url += `&endblock=${endblock}`
+  let url =
+    baseUrl +
+    `/api?module=account&action=txlist&sort=desc` +
+    getApiKeyQueryParameter(apiKey);
+  if (address) url += `&address=${address}`;
+  if (offset) url += `&offset=${offset}`;
+  if (page) url += `&page=${page}`;
+  if (startblock) url += `&startblock=${startblock}`;
+  if (endblock) url += `&endblock=${endblock}`;
 
   const responseJson = await (await fetch(url)).json();
-  const ethTransactions: ETHTransactionInfo[] = responseJson.result;
+  let ethTransactions: ETHTransactionInfo[] = responseJson.result;
+
+  const internalUrl = url.replace('txlist', 'txlistinternal');
+  const responseInternalJson = await (await fetch(internalUrl)).json();
+  const internalTransactions: ETHTransactionInfo[] = responseInternalJson.result;
+
+  if (!!internalTransactions.length) {
+    // Adds internal transactions and sorts by timestamp
+    ethTransactions = ethTransactions
+      .concat(internalTransactions)
+      .sort((txA, txB) => (txA.timeStamp > txB.timeStamp ? 1 : -1));
+  }
 
   return filterSelfTxs(ethTransactions)
     .filter((tx) => !bn(tx.value).isZero())
-    .map(getTxFromEthTransaction)
-}
+    .map(getTxFromEthTransaction);
+};
