334409ae362c82b794e267d6c4a4e33276de78bf
Juan Olmedo <juan.olmedo@constellationnetwork.io>
2023-07-18 15:52:23 -0300

    Added: getParamsFromObject function

--- source/scripts/Background/controllers/EVMChainController/etherscanApi.ts
+++ source/scripts/Background/controllers/EVMChainController/etherscanApi.ts
@@ -11,17 +11,20 @@ import {
   getTxFromEthTransaction,
   getTxFromTokenTransaction,
 } from './utils';
-
-const getApiKeyQueryParameter = (apiKey?: string): string =>
-  !!apiKey ? `&apiKey=${apiKey}` : '';
+import { getParamsFromObject } from 'utils/objects';
 
 export const getGasOracle = async (
   baseUrl: string,
   apiKey?: string
 ): Promise<GasOracleResponse> => {
-  const url = baseUrl + '/api?module=gastracker&action=gasoracle';
+  const params = {
+    module: 'gastracker',
+    action: 'gasoracle',
+    apiKey,
+  };
+  const url = baseUrl + '/api?' + getParamsFromObject(params);
 
-  const responseJson = await (await fetch(url + getApiKeyQueryParameter(apiKey))).json();
+  const responseJson = await (await fetch(url)).json();
   return responseJson.result;
 };
 
@@ -35,16 +38,24 @@ export const getTokenTransactionHistory = async ({
   endblock,
   apiKey,
 }: TransactionHistoryParam & { baseUrl: string; apiKey?: string }): Promise<Txs> => {
+  const initialParams = {
+    module: 'account',
+    action: 'tokentx',
+    sort: 'desc',
+  };
   let url =
     baseUrl +
-    `/api?module=account&action=tokentx&sort=desc` +
-    getApiKeyQueryParameter(apiKey);
-  if (address) url += `&address=${address}`;
-  if (assetAddress) url += `&contractaddress=${assetAddress}`;
-  if (offset) url += `&offset=${offset}`;
-  if (page) url += `&page=${page}`;
-  if (startblock) url += `&startblock=${startblock}`;
-  if (endblock) url += `&endblock=${endblock}`;
+    `/api?` +
+    getParamsFromObject({
+      ...initialParams,
+      address,
+      contractaddress: assetAddress,
+      offset,
+      page,
+      startblock,
+      endblock,
+      apiKey,
+    });
 
   const responseJson = await (await fetch(url)).json();
   const tokenTransactions: TokenTransactionInfo[] = responseJson.result;
@@ -66,15 +77,23 @@ export const getETHTransactionHistory = async ({
   endblock,
   apiKey,
 }: TransactionHistoryParam & { baseUrl: string; apiKey?: string }): Promise<Txs> => {
+  const initialParams = {
+    module: 'account',
+    action: 'txlist',
+    sort: 'desc',
+  };
   let url =
     baseUrl +
-    `/api?module=account&action=txlist&sort=desc` +
-    getApiKeyQueryParameter(apiKey);
-  if (address) url += `&address=${address}`;
-  if (offset) url += `&offset=${offset}`;
-  if (page) url += `&page=${page}`;
-  if (startblock) url += `&startblock=${startblock}`;
-  if (endblock) url += `&endblock=${endblock}`;
+    '/api?' +
+    getParamsFromObject({
+      ...initialParams,
+      address,
+      offset,
+      page,
+      startblock,
+      endblock,
+      apiKey,
+    });
 
   const responseJson = await (await fetch(url)).json();
   let ethTransactions: ETHTransactionInfo[] = responseJson.result;
