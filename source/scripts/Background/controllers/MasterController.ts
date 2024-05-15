cbc8fb4bf8d56fc9c2716b51900ff5ca9241ca55
Juan David Suarez <juansuarezespitia@gmail.com>
2024-05-15 15:46:33 -0500

    Removed: unused references to providers

--- source/scripts/Background/controllers/MasterController.ts
+++ source/scripts/Background/controllers/MasterController.ts
@@ -1,5 +1,3 @@
-import { EVMProvider } from 'scripts/Provider/evm';
-import { ConstellationProvider } from 'scripts/Provider/constellation';
 import WalletController from './WalletController';
 import ControllerUtils, { IControllerUtils } from './ControllerUtils';
 import ContactsController, { IContactsController } from './ContactsController';
@@ -7,33 +5,18 @@ import MigrationController from './MigrationController';
 import DAppController from './DAppController';
 
 class MasterController {
-  #stargazerProvider: ConstellationProvider;
-  #ethereumProvider: EVMProvider;
   #wallet: typeof WalletController;
   #dapp: DAppController;
-
   #contacts: IContactsController;
   #utils: IControllerUtils;
 
   constructor() {
-    this.#stargazerProvider = new ConstellationProvider();
-    this.#ethereumProvider = new EVMProvider();
     this.#wallet = WalletController;
     this.#dapp = new DAppController();
-
     this.#contacts = ContactsController();
-
     this.#utils = ControllerUtils();
 
     MigrationController();
-  }
-
-  get stargazerProvider() {
-    return this.#stargazerProvider;
-  }
-
-  get ethereumProvider() {
-    return this.#ethereumProvider;
   }
 
   get wallet() {
