d1e5ef4221e35eaf00ae289c59a8427cdec9fd48
Juan David Suarez <juansuarezespitia@gmail.com>
2024-05-15 09:13:05 -0500

    Added: new msg context for chain request handler interface

--- source/scripts/common/iRpcChainRequestHandler.ts
+++ source/scripts/common/iRpcChainRequestHandler.ts
@@ -1,13 +1,15 @@
-import { StargazerRequest } from './message_broker_utils';
+import { StargazerRequest, StargazerRequestMessage } from './message_broker_utils';
 
 type IRpcChainRequestHandler = {
   handleProxiedRequest: (
     request: StargazerRequest & { type: 'rpc' },
+    message: StargazerRequestMessage,
     sender: chrome.runtime.MessageSender
   ) => Promise<any>;
 
   handleNonProxiedRequest: (
     request: StargazerRequest & { type: 'rpc' },
+    message: StargazerRequestMessage,
     sender: chrome.runtime.MessageSender
   ) => Promise<any>;
 };
