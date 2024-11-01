60cbd2fb8d5fda224af5450f657ccd08cab77d67
Juan Olmedo <juan.olmedo@constellationnetwork.io>
2024-11-01 13:13:25 -0300

    Updated: token refresh when request is rejected

--- source/utils/httpRequests/apis.ts
+++ source/utils/httpRequests/apis.ts
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
-import { interceptor } from './interceptors';
+import { interceptorRequest, interceptorResponseRejected } from './interceptors';
 import { EXTERNAL_REQUESTS_BASE_URL } from 'constants/index';
 
 const ExplorerApi = axios.create({
   baseURL: EXTERNAL_REQUESTS_BASE_URL,
 });
 
-ExplorerApi.interceptors.request.use(interceptor);
+ExplorerApi.interceptors.request.use(interceptorRequest);
+ExplorerApi.interceptors.response.use(
+  (response) => response,
+  interceptorResponseRejected
+);
 
 export { ExplorerApi };
--- source/utils/httpRequests/interceptors.ts
+++ source/utils/httpRequests/interceptors.ts
@@ -1,4 +1,4 @@
-import axios, { InternalAxiosRequestConfig, HttpStatusCode } from 'axios';
+import axios, { InternalAxiosRequestConfig, HttpStatusCode, AxiosError } from 'axios';
 import { GetLatticeTokenResponse } from './types';
 import {
   X_LATTICE_API_KEY,
@@ -9,21 +9,21 @@ import {
 import { LATTICE_API_KEY } from 'utils/envUtil';
 import { EXTERNAL_REQUESTS_BASE_URL } from 'constants/index';
 import { generateHmac } from './signature';
-import store from 'state/store';
 import { setExternalToken } from 'state/auth';
-import { fromUnixTime, isAfter } from 'date-fns';
+import store from 'state/store';
 
-const tokenExpired = () => {
-  const { external } = store.getState().auth;
+let isRefreshing = false;
+let subscribers: any[] = [];
 
-  if (!external || !external?.token) return true;
+// Function to add a new subscriber
+const subscribeTokenRefresh = (callback: any) => {
+  subscribers.push(callback);
+};
 
-  const { exp } = external;
-
-  const expirationDate = fromUnixTime(exp);
-  const currentDate = new Date();
-
-  return isAfter(currentDate, expirationDate);
+// Function to trigger all subscribers with the new token
+const onRefreshed = (newToken: string, signature: string) => {
+  subscribers.forEach((callback) => callback(newToken, signature));
+  subscribers = [];
 };
 
 const requestToken = async () => {
@@ -46,23 +46,30 @@ const requestToken = async () => {
   return token;
 };
 
-const interceptor = async (
-  config: InternalAxiosRequestConfig<any>
-): Promise<InternalAxiosRequestConfig<any>> => {
-  const { external } = store.getState().auth;
-
-  const isExpired = tokenExpired();
-  let token = external?.token ?? '';
-
-  if (isExpired) {
-    token = await requestToken();
-  }
-
+const generateSignature = async (
+  config: InternalAxiosRequestConfig,
+  token: string
+): Promise<string> => {
   const service = config.url.split('/')[0];
   const path = '/api';
   const searchParams = `?${config.url.split('?')[1]}`;
 
   const signature = await generateHmac(token, service, path, searchParams);
+  return signature;
+};
+
+const interceptorRequest = async (
+  config: InternalAxiosRequestConfig<any>
+): Promise<InternalAxiosRequestConfig<any>> => {
+  const { external } = store.getState().auth;
+
+  let token = external?.token ?? '';
+
+  if (!token) {
+    token = await requestToken();
+  }
+
+  const signature = await generateSignature(config, token);
 
   config.headers[X_LATTICE_REQ_TOKEN] = token;
   config.headers[X_LATTICE_SIG] = signature;
@@ -70,4 +77,47 @@ const interceptor = async (
   return config;
 };
 
-export { interceptor };
+const interceptorResponseRejected = async (error: AxiosError): Promise<any> => {
+  const { status, config } = error;
+
+  if (status === HttpStatusCode.Forbidden) {
+    if (!isRefreshing) {
+      isRefreshing = true;
+
+      try {
+        // Request a new token
+        const newToken = await requestToken();
+        if (!newToken) throw new Error('Failed to refresh token');
+
+        // Generate the new signature
+        const signature = await generateSignature(config, newToken);
+        if (!signature) throw new Error('Failed to generate signature');
+
+        // Notify all subscribers that the token is refreshed
+        onRefreshed(newToken, signature);
+        isRefreshing = false;
+
+        // Retry the original request with the new token
+        config.headers[X_LATTICE_REQ_TOKEN] = newToken;
+        config.headers[X_LATTICE_SIG] = signature;
+        return axios(config);
+      } catch (refreshError) {
+        isRefreshing = false;
+        return Promise.reject(refreshError);
+      }
+    }
+
+    // If a token refresh is already in progress, queue the request
+    return new Promise((resolve) => {
+      subscribeTokenRefresh(async (newToken: string, signature: string) => {
+        config.headers[X_LATTICE_REQ_TOKEN] = newToken;
+        config.headers[X_LATTICE_SIG] = signature;
+        resolve(axios(config));
+      });
+    });
+  }
+
+  return Promise.reject(error);
+};
+
+export { interceptorRequest, interceptorResponseRejected };
