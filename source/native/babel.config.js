d6666e407d315f5fd7caa6945d60d5977fb4d188
Juan Olmedo <juan.olmedo@constellationnetwork.io>
2025-04-02 16:17:53 -0300

    Removed: brotli-wasm from react-native

--- source/native/babel.config.js
+++ source/native/babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          'brotli-wasm': './brotli-wasm-react-native.js', // Replace with our own module
+        },
+      },
+    ],
+  ],
 };
