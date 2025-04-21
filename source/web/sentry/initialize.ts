994816bc6f898e2bc9ac8a28524b48884d4d1b5f
Juan Olmedo <juan.olmedo@constellationnetwork.io>
2025-04-21 10:49:39 -0300

    v5.3.1 - Chrome

--- source/web/sentry/initialize.ts
+++ source/web/sentry/initialize.ts
@@ -13,7 +13,7 @@ const integrations = getDefaultIntegrations({}).filter((defaultIntegration) => {
   );
 });
 
-const RELEASE_NAME = `stargazer-wallet-web@5.3.0`;
+const RELEASE_NAME = `stargazer-wallet-web@5.3.1`;
 
 const client = new BrowserClient({
   dsn: process.env.SENTRY_DNS_WEB,
