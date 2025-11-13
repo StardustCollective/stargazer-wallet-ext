80f46e4d9637a2bd85e4d9f335055515f0d770fd
Juan Olmedo <juan.olmedo@constellationnetwork.io>
2025-11-13 15:56:33 -0300

    feat: import react-native-gesture-handler & update dependencies

--- source/native/android/app/src/main/java/com/stargazer/MainActivity.kt
+++ source/native/android/app/src/main/java/com/stargazer/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ReactActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
       SplashScreen.show(this)
-      super.onCreate(savedInstanceState)
+      super.onCreate(null)
   }
 
   /**
